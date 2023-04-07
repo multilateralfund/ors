aec86756fff9e0b3b9408fdb1f20372ceeb6d0a9
Iulian <iulian.safta@gmail.com>
2023-04-07 16:21:28 +0200

    deploy on staging github actions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,12 +3,16 @@
 set -e
 RUN_COMMAND="run"
 
-wait_for_services.sh
+while ! nc -z "$POSTGRES_HOST" 5432; do
+  echo "Waiting for PostgreSQL server at 5432 to accept connections on port 5432..."
+  sleep 1s
+done
 
 if [ "$DJANGO_MIGRATE" = "yes" ]; then
     python manage.py migrate --noinput
-    python manage.py collectstatic --noinput
 fi
+
+python manage.py collectstatic --noinput
 
 if [[ "$RUN_COMMAND" == *"$1"* ]]; then
     gunicorn multilateralfund.wsgi --bind 0.0.0.0:8000
--- docker/wait_for_services.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# wait-for-it "$POSTGRES_HOST:-postgres":"${POSTGRES_PORT:-5432}" --timeout=60
