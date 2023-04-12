f6d06cb0a7e2750de65426c548177abf0b7a83f9
Iulian <iulian.safta@gmail.com>
2023-04-12 10:28:26 +0200

    added vite as frontend framework

--- multilateralfund/settings.py
+++ multilateralfund/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "api/static/"
 STATIC_ROOT = "static"
 
 # Default primary key field type
--- multilateralfund/urls.py
+++ multilateralfund/urls.py
@@ -20,5 +20,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
