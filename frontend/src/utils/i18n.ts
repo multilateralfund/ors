b2ca9c5ffcd461a5f73bcb8314e2c8af512c0e14
Iulian Safta <iulian.safta@gmail.com>
2023-05-04 10:25:10 +0200

    remove comments

--- frontend/src/utils/i18n.ts
+++ frontend/src/utils/i18n.ts
@@ -3,9 +3,6 @@ import { initReactI18next } from 'react-i18next'
 import HttpBackend, { HttpBackendOptions } from 'i18next-http-backend'
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
   .use(HttpBackend)
   .use(initReactI18next)
   .init<HttpBackendOptions>({
