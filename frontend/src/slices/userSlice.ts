878fe0ef8de4f2fb7a6dba34cacea7095ad7da1a
Iulian Safta <iulian.safta@gmail.com>
2023-05-09 20:06:59 +0200

    remember selected language

--- frontend/src/slices/userSlice.ts
+++ frontend/src/slices/userSlice.ts
@@ -1,15 +1,18 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { IUser } from '@/types/User'
 import { RootState } from '../store'
+import { LanguagesKeys } from '@/utils/constants'
 
 interface IUserState {
   user: IUser | null
   theme: string | undefined
+  lang: LanguagesKeys
 }
 
 const initialState: IUserState = {
   user: null,
   theme: undefined,
+  lang: 'en',
 }
 
 export const userSlice = createSlice({
@@ -25,11 +28,19 @@ export const userSlice = createSlice({
       }
       state.theme = action.payload.mode
     },
+    setLang: (state, action: PayloadAction<{ lang: string }>) => {
+      if (action.payload.lang) {
+        localStorage.setItem('lang', action.payload.lang)
+      }
+      state.theme = action.payload.lang
+    },
   },
 })
 
-export const { setUser, setTheme } = userSlice.actions
+export const { setUser, setTheme, setLang } = userSlice.actions
 
 export const selectUser = (state: RootState) => state.user
+export const selectLang = (state: RootState) =>
+  localStorage.getItem('lang') || state.user.lang
 
 export const userReducer = userSlice.reducer
