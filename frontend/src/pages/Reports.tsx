6f7e3d3f38ca893b3423c0ba0aa1c9972b70e1a1
Iulian Safta <iulian.safta@gmail.com>
2023-05-29 12:33:05 +0200

    added tab list

--- frontend/src/pages/Reports.tsx
+++ frontend/src/pages/Reports.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react'
 import { TableData } from '@/components/table/TableData'
+import { Tabs } from 'flowbite-react'
 import { useGetSubstancesQuery, useGetUsageQuery } from '@/services/api'
 
 export const ReportsPage: FC = function () {
@@ -7,8 +8,17 @@ export const ReportsPage: FC = function () {
   useGetUsageQuery(null)
 
   return (
-    <div>
-      <TableData />
+    <div className="mt-2">
+      <Tabs.Group aria-label="" style="fullWidth">
+        <Tabs.Item title="Section A">
+          <TableData />
+        </Tabs.Item>
+        <Tabs.Item title="Section B">Section B</Tabs.Item>
+        <Tabs.Item title="Section C">Section C</Tabs.Item>
+        <Tabs.Item title="Section D">Section D</Tabs.Item>
+        <Tabs.Item title="Section E">Section E</Tabs.Item>
+        <Tabs.Item title="Section F">Section F</Tabs.Item>
+      </Tabs.Group>
     </div>
   )
 }
