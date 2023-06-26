14ae95cf7c7a2c203c9d568e54a37898ee67c7a6
gabrielastrezea <strezea_gabriela@yahoo.com>
2023-06-26 12:25:07 +0300

    Rename api docs, add it to admin and readme. Fix scheme problem. (#54)
    * Rename api docs, add it to admin and readme. Fix scheme problem.
    
    * More details in readme
    
    * Without protocols in allowed host

--- core/api/urls.py
+++ core/api/urls.py
@@ -19,7 +19,6 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Multilateral Fund API",
         default_version="v1",
-        description="API docs for mlf",
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
@@ -27,17 +26,14 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
+        r"^docs(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
     re_path(
-        r"^swagger/$",
+        r"^docs/$",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
-    ),
-    re_path(
-        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
     path("usages/", UsageListAPIView.as_view(), name="usages-list"),
     path(
