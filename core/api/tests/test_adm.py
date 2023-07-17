241223f894cbf926d5aeb012e624249b458e4a2f
Mereu Alexandra <mereu.alexandra@yahoo.com>
2023-07-17 09:11:39 +0300

    Added pytest fixtures (#64)

--- core/api/tests/test_adm.py
+++ core/api/tests/test_adm.py
@@ -3,8 +3,6 @@ from core.api.tests.factories import (
     AdmColumnFactory,
     AdmRowFactory,
     CPReportFactory,
-    CountryFactory,
-    UserFactory,
 )
 import pytest
 from django.urls import reverse
@@ -14,107 +12,109 @@ from rest_framework.test import APIClient
 pytestmark = pytest.mark.django_db
 
 
+@pytest.fixture(name="_setup_empty_form")
+def setup_empty_form(country_ro, cp_report_2005):
+    cp_report_17 = CPReportFactory.create(
+        country=country_ro, year=2017, comment="Daca ploua nu ma ploua"
+    )
+
+    for section in ["B", "C"]:
+        # create adm column
+        for i in range(2):
+            order = 2 - i
+            AdmColumnFactory.create(
+                display_name=f"adm_column_{order}",
+                sort_order=order,
+                section=section,
+                min_year=2000,
+                max_year=2010,
+            )
+
+        # create adm rows
+        row_data = {
+            "section": section,
+            "min_year": 2000,
+            "max_year": 2010,
+        }
+        title = AdmRowFactory.create(
+            text=f"title_{section}",
+            sort_order=2,
+            type="title",
+            parent=None,
+            **row_data,
+        )
+        subtitle = AdmRowFactory.create(
+            text=f"subtitle_{section}",
+            sort_order=4,
+            type="subtitle",
+            parent=title,
+            **row_data,
+        )
+
+        row_data["type"] = "question"
+        row_data["parent"] = subtitle
+        for i in range(2):
+            order = 4 + i
+            AdmRowFactory.create(
+                text=f"{subtitle.text}_row_{i}",
+                sort_order=order,
+                **row_data,
+            )
+        if section == "B":
+            # add 1.6.1 rows
+            AdmRowFactory.create(
+                text=f"{subtitle.text}_row_161",
+                sort_order=10,
+                index="1.6.1",
+                country_programme_report=cp_report_2005,
+                **row_data,
+            )
+            AdmRowFactory.create(
+                text="N/A",
+                sort_order=12,
+                index="1.6.1",
+                **row_data,
+            )
+
+    for i in range(2):
+        d_row = AdmRowFactory.create(
+            text=f"d_row_{i}",
+            sort_order=1,
+            section="D",
+            type="question",
+            min_year=2000,
+            max_year=2010,
+        )
+        # add 3 choices for each row
+        for j in range(3):
+            AdmChoiceFactory.create(
+                adm_row=d_row,
+                value=f"d_row_choice_{i}{j}",
+                sort_order=i,
+            )
+
+    return cp_report_17
+
+
 # pylint: disable=C8008
 class TestAdm:
     client = APIClient()
 
-    def test_get_empty_form(self):
-        # create country and cp report
-        ro = CountryFactory.create(name="Romania")
-        cp_report = CPReportFactory.create(
-            country=ro, year=2005, comment="Si daca e rau, tot e bine"
-        )
-        cp_report_17 = CPReportFactory.create(
-            country=ro, year=2017, comment="Daca ploua nu ma ploua"
-        )
-
-        for section in ["B", "C"]:
-            # create adm column
-            for i in range(2):
-                order = 2 - i
-                AdmColumnFactory.create(
-                    display_name=f"adm_column_{order}",
-                    sort_order=order,
-                    section=section,
-                    min_year=2000,
-                    max_year=2010,
-                )
-
-            # create adm rows
-            row_data = {
-                "section": section,
-                "min_year": 2000,
-                "max_year": 2010,
-            }
-            title = AdmRowFactory.create(
-                text=f"title_{section}",
-                sort_order=2,
-                type="title",
-                parent=None,
-                **row_data,
-            )
-            subtitle = AdmRowFactory.create(
-                text=f"subtitle_{section}",
-                sort_order=4,
-                type="subtitle",
-                parent=title,
-                **row_data,
-            )
-
-            row_data["type"] = "question"
-            row_data["parent"] = subtitle
-            for i in range(2):
-                order = 4 + i
-                AdmRowFactory.create(
-                    text=f"{subtitle.text}_row_{i}",
-                    sort_order=order,
-                    **row_data,
-                )
-            if section == "B":
-                # add 1.6.1 rows
-                AdmRowFactory.create(
-                    text=f"{subtitle.text}_row_161",
-                    sort_order=10,
-                    index="1.6.1",
-                    country_programme_report=cp_report,
-                    **row_data,
-                )
-                AdmRowFactory.create(
-                    text="N/A",
-                    sort_order=12,
-                    index="1.6.1",
-                    **row_data,
-                )
-
-        for i in range(2):
-            d_row = AdmRowFactory.create(
-                text=f"d_row_{i}",
-                sort_order=1,
-                section="D",
-                type="question",
-                min_year=2000,
-                max_year=2010,
-            )
-            # add 3 choices for each row
-            for j in range(3):
-                AdmChoiceFactory.create(
-                    adm_row=d_row,
-                    value=f"d_row_choice_{i}{j}",
-                    sort_order=i,
-                )
+    def test_get_empty_form(self, user, cp_report_2005, _setup_empty_form):
+        cp_report_17 = _setup_empty_form
 
         # get adm form
         url = reverse("adm-empty-form")
 
         # test without authentication
-        response = self.client.get(url, {"cp_report_id": cp_report.id})
+        response = self.client.get(url, {"cp_report_id": cp_report_2005.id})
         assert response.status_code == 403
 
         # test with authentication
-        self.client.force_authenticate(user=UserFactory())
+        self.client.force_authenticate(user=user)
 
         # get adm form for 2005 cp report
-        response = self.client.get(url, {"cp_report_id": cp_report.id})
+        response = self.client.get(url, {"cp_report_id": cp_report_2005.id})
         assert response.status_code == 200
         assert len(response.data["admB"]["columns"]) == 2
         assert len(response.data["admB"]["rows"]) == 5
