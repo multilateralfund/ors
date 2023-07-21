531231c4aa2709e92f860ecfc94ccb39cae4db57
Miu Razvan <miu.razvan28@gmail.com>
2023-07-21 16:22:30 +0300

    wip

--- frontend/src/components/manage/Widgets/PasswordWidget.tsx
+++ frontend/src/components/manage/Widgets/PasswordWidget.tsx
@@ -9,7 +9,13 @@ import TextWidget from '@ors/components/manage/Widgets/TextWidget'
 import { IoEye } from '@react-icons/all-files/io5/IoEye'
 import { IoEyeOff } from '@react-icons/all-files/io5/IoEyeOff'
 
-export default function PasswordWidget(props: TextWidgetProps): JSX.Element {
+export type PasswordWidgetProps = TextWidgetProps
+
+export type PasswordWidget = (props: PasswordWidgetProps) => JSX.Element
+
+export default function PasswordWidget(
+  props: PasswordWidgetProps,
+): JSX.Element {
   const [showPassword, setShowPassword] = React.useState(false)
 
   function handleClickShowPassword() {
