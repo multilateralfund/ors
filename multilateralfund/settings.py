7619cdf32b9e45d5f083d920d7928f35f69ca0d9
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-07-26 16:40:11 +0300

    Typo

--- multilateralfund/settings.py
+++ multilateralfund/settings.py
@@ -33,7 +33,7 @@ FRONTEND_HOST = env.list("FRONTEND_HOST", default=["http://localhost:3000"])
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("DJANGO_SECRET_KEY", default="")
-ca
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
