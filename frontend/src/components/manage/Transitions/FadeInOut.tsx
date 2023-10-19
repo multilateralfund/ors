02f01cd0ca340a47ac3c5d3473dc3d7902e0f157
Miu Razvan <miu.razvan28@gmail.com>
2023-10-19 12:25:59 +0300

    fix: smooth animation fix for firefox

--- frontend/src/components/manage/Transitions/FadeInOut.tsx
+++ frontend/src/components/manage/Transitions/FadeInOut.tsx
@@ -1,39 +1,29 @@
 'use client'
 
-import type { AnyObject } from '@ors/types/primitives'
 import type { HTMLMotionProps } from 'framer-motion'
-
-import { ReactHTML } from 'react'
 
 import cx from 'classnames'
 import { LazyMotion, domAnimation, m } from 'framer-motion'
-import { get } from 'lodash'
 
-export type FadeInOutProps = HTMLMotionProps<keyof ReactHTML> & {
-  [key: string]: any
-  FadeInOut?: AnyObject & { component: string }
-}
+export type FadeInOutProps = HTMLMotionProps<'div'>
 
 export default function FadeInOut({
-  FadeInOut,
   children,
   className,
   ...rest
 }: FadeInOutProps) {
-  const Motion = get(m, FadeInOut?.component || 'div')
-
   return (
     <LazyMotion features={domAnimation} strict>
-      <Motion
+      <m.div
         initial={{ opacity: 0 }}
         exit={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.3 }}
-        className={cx('motion fade-in-out', className)}
+        className={cx('fade-in-out', className)}
         {...rest}
       >
         {children}
-      </Motion>
+      </m.div>
     </LazyMotion>
   )
 }
