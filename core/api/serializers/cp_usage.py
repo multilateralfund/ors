6b57f4ee6fe0b2c6195512a902dc9cfefc59a2ef
Mereu Alexandra <mereu.alexandra@yahoo.com>
2023-10-23 10:57:22 +0300

    Updated cp serializer/view structure (#112)
    * Updated cp serializer/view structure
    
    * Pylint

---
+++ core/api/serializers/cp_usage.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from core.models.country_programme import CPUsage
+from core.models.usage import Usage
+
+
+class CPUsageSerializer(serializers.ModelSerializer):
+    usage = serializers.StringRelatedField()
+    usage_id = serializers.PrimaryKeyRelatedField(
+        required=True,
+        queryset=Usage.objects.all().values_list("id", flat=True),
+    )
+    quantity = serializers.DecimalField(max_digits=12, decimal_places=3)
+
+    class Meta:
+        model = CPUsage
+        fields = [
+            "usage",
+            "usage_id",
+            "quantity",
+        ]
+
+    def to_internal_value(self, data):
+        try:
+            intenal_value = super().to_internal_value(data)
+        except ValidationError as e:
+            # add usage_id to error message
+            row_id = data.get("usage_id", "general_error")
+            if row_id != "general_error":
+                row_id = f"usage_{row_id}"
+            raport_error = {
+                "row_id": row_id,
+                "errors": e.detail,
+            }
+            raise ValidationError(raport_error) from e
+
+        return intenal_value
