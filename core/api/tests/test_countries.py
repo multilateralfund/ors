20f101fc786f53226859a1890c788e291a4ee358
alexkiro <1538458+alexkiro@users.noreply.github.com>
2023-10-31 17:23:37 +0200

    Backend/filter countries (#115)
    * Add the ability to filter countries on cp report
    
    * Add tests for the new filter

--- core/api/tests/test_countries.py
+++ core/api/tests/test_countries.py
@@ -4,7 +4,7 @@ import pytest
 from django.urls import reverse
 
 pytestmark = pytest.mark.django_db
-# pylint: disable=C8008
+# pylint: disable=C8008,W0613
 
 
 @pytest.fixture(name="_setup_countries")
@@ -23,3 +23,23 @@ class TestCountries(BaseTest):
         assert len(response.data) == 2
         assert response.data[0]["name"] == "France"
         assert response.data[1]["name"] == country_ro.name
+
+    def test_countries_with_cp_report(
+        self, user, country_ro, _setup_countries, cp_report_2005
+    ):
+        self.client.force_authenticate(user=user)
+
+        response = self.client.get(self.url, {"with_cp_report": "true"})
+        assert response.status_code == 200
+        assert len(response.data) == 1
+        assert response.data[0]["name"] == country_ro.name
+
+    def test_countries_without_cp_report(
+        self, user, country_ro, _setup_countries, cp_report_2005
+    ):
+        self.client.force_authenticate(user=user)
+
+        response = self.client.get(self.url, {"with_cp_report": "false"})
+        assert response.status_code == 200
+        assert len(response.data) == 1
+        assert response.data[0]["name"] == "France"
--- core/api/utils.py
+++ core/api/utils.py
@@ -1,3 +1,7 @@
+from django.db.models import Exists
+from django.db.models import OuterRef
+from django_filters import rest_framework as filters
+
 SECTION_ANNEX_MAPPING = {
     "A": ["A", "B", "C", "D", "E"],
     "B": ["F"],
@@ -5,3 +9,39 @@ SECTION_ANNEX_MAPPING = {
 }
 
 SUBMISSION_STATUSE_CODES = ["NEWSUB", "UNK"]
+
+
+class RelatedExistsFilter(filters.BooleanFilter):
+    """Filter query based on whether it has at least one row in the specified related field."""
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault(
+            "help_text",
+            (
+                f"If true list only entries that have at least one related {kwargs['field_name']}; "
+                f"if false list entries without any related rows."
+            ),
+        )
+        super().__init__(*args, **kwargs)
+
+    def filter(self, qs, value):
+        if value is None:
+            return qs
+
+        # The relationship between the two models used for filtering
+        rel = getattr(qs.model, self.field_name).rel
+        # The related model
+        related_model = rel.related_model
+        # Related field pk usually "id"
+        related_field_ref = rel.field_name
+        # The name of the FK in the related model, used for filtering
+        related_field_name = rel.field.name
+
+        subquery = Exists(
+            related_model.objects.filter(
+                **{related_field_name: OuterRef(related_field_ref)}
+            )
+        )
+        if value:
+            return qs.filter(subquery)
+        return qs.exclude(subquery)
