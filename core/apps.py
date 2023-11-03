ce1586ad632496956ea5dcce9f01e26792d62001
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-11-03 11:01:35 +0200

    Add printer user

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,24 @@
+import logging
+
 from django.apps import AppConfig
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+
+logger = logging.getLogger(__name__)
+
+
+def create_printer_user():
+    User = get_user_model()
+    obj, created = User.objects.get_or_create(username=settings.PRINTER_USERNAME)
+    obj.set_password(settings.PRINTER_USER_PASSWORD)
+    obj.save()
+    logger.debug("Printer user %s configured: created=%s", obj, created)
 
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+    def ready(self):
+        create_printer_user()
