5292bd8cdaa7b821da000e01855569328cce68d9
Mereu Alexandra <mereu.alexandra@yahoo.com>
2023-12-14 17:12:56 +0200

    Update country model (#137)
    * Added regions and subregions in country
    
    * Removed region/subregion

--- core/admin/country.py
+++ core/admin/country.py
@@ -1,34 +1,7 @@
 from django.contrib import admin
 
 from core.admin.utils import get_final_display_list
-from core.models.country import Country, Region, Subregion
-
-
-@admin.register(Region)
-class RegionAdmin(admin.ModelAdmin):
-    search_fields = [
-        "name",
-    ]
-
-    def get_list_display(self, request):
-        exclude = ["subregion"]
-        return get_final_display_list(Region, exclude)
-
-
-@admin.register(Subregion)
-class SubregionAdmin(admin.ModelAdmin):
-    search_fields = [
-        "name",
-    ]
-    list_filter = ["region"]
-
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        return queryset.select_related("region")
-
-    def get_list_display(self, request):
-        exclude = ["country"]
-        return get_final_display_list(Subregion, exclude)
+from core.models.country import Country
 
 
 @admin.register(Country)
@@ -41,12 +14,8 @@ class CountryAdmin(admin.ModelAdmin):
         "abbr",
         "abbr_alt",
     ]
-    list_filter = ["subregion", "subregion__region"]
-
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        return queryset.select_related("subregion__region")
+    list_filter = ["location_type"]
 
     def get_list_display(self, request):
-        exclude = ["cpreport", "project", "bprecord", "cpreportarchive"]
+        exclude = ["cpreport", "project", "bprecord", "cpreportarchive", "country"]
         return get_final_display_list(Country, exclude)
--- core/models/country.py
+++ core/models/country.py
@@ -13,6 +13,11 @@ class CountryManager(models.Manager):
 
 # country model; contains name, m49 code, and iso code
 class Country(models.Model):
+    class LocationType(models.TextChoices):
+        COUNTRY = "Country", "Country"
+        REGION = "Region", "Region"
+        SUBREGION = "Subregion", "Subregion"
+
     name = models.CharField(max_length=100, unique=True)
     name_alt = models.CharField(max_length=256, null=True, blank=True)
     abbr = models.CharField(max_length=10, null=True, blank=True)
@@ -23,49 +28,17 @@ class Country(models.Model):
     is_lvc = models.BooleanField(default=False)
     lvc_baseline = models.FloatField(null=True, blank=True)
     ozone_id = models.IntegerField(null=True, blank=True)
-    subregion = models.ForeignKey(
-        "Subregion",
-        null=True,
-        blank=True,
-        on_delete=models.CASCADE,
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+    location_type = models.CharField(
+        max_length=16, choices=LocationType.choices, default=LocationType.COUNTRY
     )
+    import_id = models.IntegerField(null=True, blank=True)
 
     objects = CountryManager()
 
     class Meta:
         verbose_name_plural = "Countries"
         ordering = ("name",)
-
-    def __str__(self):
-        return self.name
-
-
-class RegionManager(models.Manager):
-    def find_by_name(self, name):
-        name_str = name.strip()
-        return self.filter(name__iexact=name_str).first()
-
-
-class Region(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    abbr = models.CharField(max_length=10, null=True, blank=True)
-
-    objects = RegionManager()
-
-    def __str__(self):
-        return self.name
-
-
-class Subregion(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    abbr = models.CharField(max_length=10, null=True, blank=True)
-    import_id = models.IntegerField(null=True, blank=True)
-    region = models.ForeignKey(
-        Region,
-        on_delete=models.CASCADE,
-    )
-
-    objects = RegionManager()
 
     def __str__(self):
         return self.name
