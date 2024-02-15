f74acb8fa79977622114b71aee1b4996a8c97295
Mereu Alexandra <mereu.alexandra@yahoo.com>
2024-02-15 11:18:44 +0200

    Added has_cp_report field to country json (#149)
    * Added has_cp_report field
    
    * Added tests

--- core/api/serializers/country.py
+++ core/api/serializers/country.py
@@ -4,6 +4,11 @@ from core.models import Country
 
 
 class CountrySerializer(serializers.ModelSerializer):
+    has_cp_report = serializers.SerializerMethodField()
+
     class Meta:
         model = Country
-        fields = ["id", "name", "abbr", "name_alt", "iso3"]
+        fields = ["id", "name", "abbr", "name_alt", "iso3", "has_cp_report"]
+
+    def get_has_cp_report(self, obj):
+        return getattr(obj, "has_cp_report", None)
