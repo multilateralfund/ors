93c1be319cb15b92e98128905cfb92a9dd962bb2
GeorgeFlorian <georgeflorian12@gmail.com>
2024-04-03 15:00:12 +0300

    Add Report info to tab list

---
+++ frontend/src/components/manage/Blocks/Section/ReportInfo/View/View.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReportInfoView = () => {
+  return (
+    <div>
+      {/* Your JSX for the "Report info" section */}
+      Report Info Section
+    </div>
+  );
+};
+
+export default ReportInfoView;
