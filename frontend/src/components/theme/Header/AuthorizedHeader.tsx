d44bf1f8ada799e4f050beea3711120e64f51fa0
David Batranu <david.batranu@eaudeweb.ro>
2024-04-03 20:41:58 +0300

    [refs #23956] (WIP) New header.

--- frontend/src/components/theme/Header/AuthorizedHeader.tsx
+++ frontend/src/components/theme/Header/AuthorizedHeader.tsx
@@ -1,24 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
-import { Box, Button, Divider, Typography } from '@mui/material'
+import { useState } from 'react'
+
+import { Box } from '@mui/material'
 import cx from 'classnames'
 import { AnimatePresence } from 'framer-motion'
-import { isFunction } from 'lodash'
-import { usePathname } from 'next/navigation'
-import { useRouter } from 'next/navigation'
+import { DebouncedFunc, debounce, isFunction } from 'lodash'
+import { usePathname, useRouter } from 'next/navigation'
 
 // import CollapseInOut from '@ors/components/manage/Transitions/CollapseInOut'
 import FadeInOut from '@ors/components/manage/Transitions/FadeInOut'
 import Logo from '@ors/components/theme/Logo/Logo'
-import ProfileDropdown from '@ors/components/theme/Profile/ProfileDropdown'
 import Dropdown from '@ors/components/ui/Dropdown/Dropdown'
 // import ThemeSelector from '@ors/components/theme/ThemeSelector/ThemeSelector'
 import UnstyledLink, { LinkProps } from '@ors/components/ui/Link/Link'
-import { formatApiUrl } from '@ors/helpers/Api/Api'
 import { matchPath } from '@ors/helpers/Url/Url'
 import { useStore } from '@ors/store'
 
-import { IoChevronDown, IoSettingsOutline } from 'react-icons/io5'
+import { IoChevronDown } from 'react-icons/io5'
 
 function Link({
   children,
@@ -46,6 +45,119 @@ function Link({
     >
       {children}
     </UnstyledLink>
+  )
+}
+
+const HeaderNavigation = () => {
+  const [showMenu, setShowMenu] = useState<Record<string, boolean>>({})
+  const [hideInProgress, setHideInProgress] =
+    useState<DebouncedFunc<any> | null>(null)
+
+  const handleShowMenu = (label: string) => {
+    hideInProgress?.cancel()
+    setShowMenu(() => ({ [label]: true }))
+  }
+
+  const handleHideAllMenus = () => {
+    const eta = debounce(() => setShowMenu(() => ({})), 300)
+    setHideInProgress(() => eta)
+    eta()
+  }
+
+  const baseUrl = 'https://prod.multilateralfund.edw.ro'
+
+  const items = [
+    {
+      label: 'About MLF',
+      menu: [
+        { label: 'Governance', url: '/about/governance' },
+        { label: 'History', url: '/about/history' },
+        { label: 'Mission', url: '/about/mission' },
+        {
+          label: 'Monitoring & Evaluation',
+          url: '/about/monitoring-evaluation',
+        },
+        { label: 'Partners', url: '/about/partners' },
+        { label: 'Secretariat', url: '/about/secretariat' },
+      ],
+      url: '/about/',
+    },
+    { label: 'Our impact', url: '/our-impact' },
+    {
+      label: 'Projects & Data',
+      menu: [
+        {
+          label: 'Countries Dashboard',
+          url: '/projects-data/countries-dashboard',
+        },
+        { label: 'Funding Dashboard', url: '/projects-data/funding-dashboard' },
+        {
+          label: 'People & Environment',
+          url: '/projects-data/people-environment',
+        },
+        {
+          label: 'Projects Dashboard',
+          url: '/projects-data/projects-dashboard',
+        },
+      ],
+      url: '/projects-data',
+    },
+    {
+      label: 'Resources',
+      menu: [
+        { label: 'Decision handbook', url: '/resources/decisions' },
+        {
+          label: 'Project Guides & Tools',
+          url: '/resources/project-guides-tools',
+        },
+        { label: 'Scorecards', url: '/resources/secretariat-scorecards' },
+      ],
+      url: '/resources',
+    },
+    { label: 'Meetings', url: '/meetings' },
+    { label: 'News & Stories', url: '/news-stories' },
+  ]
+
+  return (
+    <div
+      className="flex gap-x-4 rounded-full bg-white px-5 py-3 font-medium shadow-lg"
+      onMouseLeave={handleHideAllMenus}
+    >
+      {items.map((item) => (
+        <div
+          key={item.label}
+          className="relative flex cursor-pointer text-primary"
+        >
+          <a
+            className="flex items-center justify-between gap-x-2 rounded-full px-4 py-2 text-lg text-primary no-underline hover:bg-primary hover:text-mlfs-hlYellow"
+            href={`${baseUrl}${item.url}`}
+            onMouseEnter={() => handleShowMenu(item.label)}
+          >
+            {item.label}
+            {item.menu && <IoChevronDown />}
+          </a>
+          {showMenu[item.label] && (
+            <FadeInOut className="absolute left-0 z-10">
+              <div
+                className="absolute top-12 z-10 mt-2 flex w-48 flex-col rounded-lg bg-white shadow-lg"
+                onMouseLeave={handleHideAllMenus}
+                onMouseOver={() => handleShowMenu(item.label)}
+              >
+                {item.menu?.map((menuItem) => (
+                  <a
+                    key={menuItem.label}
+                    className="border-2 border-l-0 border-r-0 border-t-0 border-solid border-b-sky-400 px-4 py-2 text-primary no-underline transition-all first:rounded-t-lg last:rounded-b-lg last:border-b-0 hover:bg-mlfs-hlYellowTint"
+                    href={`${baseUrl}${item.url}`}
+                  >
+                    {menuItem.label}
+                  </a>
+                ))}
+              </div>
+            </FadeInOut>
+          )}
+        </div>
+      ))}
+    </div>
   )
 }
 
@@ -83,38 +195,11 @@ export default function Header() {
         style={{ backgroundColor: '#e6f4fb' }}
       >
         <div className="container flex w-full items-center justify-between pb-4">
-          <div className="flex items-center gap-x-8">
+          <div className="flex w-full flex-auto items-center justify-between gap-x-8 ">
             <UnstyledLink href="/">
               <Logo className="h-[100px] w-[160px] md:w-[240px]" />
             </UnstyledLink>
-            <Divider
-              className="my-5 border-secondary md:border-2"
-              orientation="vertical"
-              variant="middle"
-              flexItem
-            />
-            <Typography
-              className="text-5xl font-bold leading-none text-typography-secondary md:text-6xl"
-              component="p"
-            >
-              Data portal
-            </Typography>
-          </div>
-          <div className="self-baseline md:self-auto">
-            {/* <LanguageSelector className="ltr:mr-2 rtl:ml-2" /> */}
-            <UnstyledLink
-              className="hidden theme-dark:text-white md:inline-block"
-              href={formatApiUrl('/admin/')}
-              button
-            >
-              <div className="flex flex-row items-center justify-between gap-x-2 text-xl font-normal uppercase">
-                <IoSettingsOutline className="text-secondary" />
-                Admin
-              </div>
-            </UnstyledLink>
-            <ProfileDropdown className="md:p-auto md:min-w-auto mr-0 min-w-0 pr-0 md:m-auto ltr:mr-2 rtl:ml-2" />
-            {/* {__DEVELOPMENT__ && <ThemeSelector />} */}
-            <div id="header-control" />
+            <HeaderNavigation />
           </div>
         </div>
         <Box
