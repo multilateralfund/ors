ebabe7d5bb77ef09d5b3f581b4cf02ff315991e4
Rares Siteavu <rares.siteavu@yahoo.ro>
2024-04-04 14:10:07 +0300

    Linting

--- core/api/permissions.py
+++ core/api/permissions.py
@@ -5,16 +5,13 @@ class IsUserAllowedCP(permissions.BasePermission):
     def has_permission(self, request, view):
         user = request.user
         if user.is_authenticated:
-            if (
-                user.user_type == user.UserType.COUNTRY_USER
-                or user.user_type == user.UserType.SECRETARIAT
+            if user.user_type in (
+                user.UserType.COUNTRY_USER,
+                user.UserType.SECRETARIAT,
             ):
                 return True
 
-            if (
-                user.user_type == user.UserType.AGENCY
-                or user.user_type == user.UserType.STAKEHOLDER
-            ):
+            if user.user_type in (user.UserType.AGENCY, user.UserType.STAKEHOLDER):
                 if request.method in permissions.SAFE_METHODS:
                     return True
         return False
