639af9bb24a393879a3fe03ddd502eafdfc2b530
GeorgeFlorian <georgeflorian12@gmail.com>
2024-04-12 15:48:16 +0300

    Improve SectionOverlay; Add overlay on comment section based on user_type

--- frontend/src/components/manage/Blocks/CountryProgramme/CPComments.tsx
+++ frontend/src/components/manage/Blocks/CountryProgramme/CPComments.tsx
@@ -4,6 +4,9 @@ import Button from '@mui/material/Button'
 import TextareaAutosize from '@mui/material/TextareaAutosize'
 import Typography from '@mui/material/Typography'
 
+import SectionOverlay from '@ors/components/ui/SectionOverlay/SectionOverlay'
+import { useStore } from '@ors/store'
+
 interface TextState {
   mlfs: string
   user: string
@@ -12,6 +15,8 @@ interface TextState {
 type Label = keyof TextState
 
 const CPComments: React.FC = () => {
+  const user = useStore((state) => state.user)
+  const user_type = user.data.user_type
   // GET initial texts from API
   const [initialTexts, setInitialTexts] = useState<TextState>({
     mlfs: 'MLFS initial comment',
@@ -54,6 +59,10 @@ const CPComments: React.FC = () => {
   }
 
   const labels: Label[] = ['user', 'mlfs']
+  const allowComments = {
+    mlfs: 'secretariat',
+    user: 'country_user',
+  }
 
   return (
     <form className="mt-4 flex w-full flex-col gap-4">
@@ -64,9 +73,14 @@ const CPComments: React.FC = () => {
         {labels.map((label) => (
           <div
             key={label}
-            className="flex min-w-96 flex-grow flex-col rounded-lg bg-gray-100 p-2"
+            className="relative flex min-w-96 flex-grow flex-col rounded-lg bg-gray-100 p-2"
           >
-            <label className="py-2 text-2xl font-normal">{label}</label>
+            {user_type !== allowComments[label] && (
+              <SectionOverlay className="cursor-not-allowed" opacity={60} />
+            )}
+            <label className="py-2 text-2xl font-normal">
+              {label.toLocaleUpperCase()}
+            </label>
             <div className="CPComments relative">
               <TextareaAutosize
                 className="w-full resize-none rounded-lg border border-solid border-gray-300 p-2 pb-6 shadow-none"
@@ -75,25 +89,27 @@ const CPComments: React.FC = () => {
                 value={texts[label]}
                 onChange={(e) => handleTextChange(label, e.target.value)}
               />
-              <div className="absolute bottom-2 right-2 flex gap-2 mb-2 opacity-50">
-                <Button
-                  color="primary"
-                  disabled={texts[label] === initialTexts[label]}
-                  size="small"
-                  variant="contained"
-                  onClick={handleSave}
-                >
-                  Save
-                </Button>
-                <Button
-                  disabled={texts[label] === initialTexts[label]}
-                  size="small"
-                  variant="outlined"
-                  onClick={() => handleCancel(label)}
-                >
-                  Cancel
-                </Button>
-              </div>
+              {user_type === allowComments[label] && (
+                <div className="absolute bottom-2 right-2 mb-2 flex gap-2 opacity-60">
+                  <Button
+                    color="primary"
+                    disabled={texts[label] === initialTexts[label]}
+                    size="small"
+                    variant="contained"
+                    onClick={handleSave}
+                  >
+                    Save
+                  </Button>
+                  <Button
+                    disabled={texts[label] === initialTexts[label]}
+                    size="small"
+                    variant="outlined"
+                    onClick={() => handleCancel(label)}
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              )}
             </div>
           </div>
         ))}
--- frontend/src/components/ui/SectionOverlay/SectionOverlay.tsx
+++ frontend/src/components/ui/SectionOverlay/SectionOverlay.tsx
@@ -1,5 +1,9 @@
-const SectionOverlay = () => {
-  return <div className="absolute inset-0 z-50 bg-gray-100 opacity-70"></div>
+const SectionOverlay = ({ className = "", opacity = 70 }) => {
+  return (
+    <div
+      className={`absolute inset-0 z-50 bg-gray-100 opacity-${opacity} ${className}`}
+    ></div>
+  )
 }
 
 export default SectionOverlay
