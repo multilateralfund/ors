97bc7b3d0a487277a14eff01c32abab5a1bd9b0e
GeorgeFlorian <georgeflorian12@gmail.com>
2024-04-15 14:06:25 +0300

    Improve CP History design

--- frontend/src/components/manage/Blocks/Section/ReportInfo/ReportHistory.tsx
+++ frontend/src/components/manage/Blocks/Section/ReportInfo/ReportHistory.tsx
@@ -1,42 +1,52 @@
+import { useStore } from '@ors/store'
+
 const ReportHistory = () => {
+  const { report } = useStore((state) => state.cp_reports)
+  // @ts-ignore
+  const { event_description, last_updated_at, last_updated_by } = report.data
+
+  const dateObject = new Date(last_updated_at)
+
+  const options: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
+    hour: 'numeric',
+    // hour12: false,
+    minute: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }
+
+  const formattedDateTime = dateObject.toLocaleDateString(undefined, options)
+
   return (
     <div>
       <p className="mb-3 text-2xl font-normal">History</p>
-      <div className="flex flex-col flex-wrap justify-center gap-2 rounded-lg bg-white px-4 py-1 shadow-lg">
-        {[...Array(3)].map((_, index) => {
-          const randomDate = new Date(
-            +new Date() - Math.floor(Math.random() * 10000000000),
-          )
-          return (
-            <div
-              key={index}
-              className="flex grow items-center justify-between gap-3 text-pretty"
+      <div className="flex flex-col flex-wrap justify-center gap-1 rounded-lg bg-white px-4 py-3 shadow-lg">
+        <div className="flex grow items-center justify-between gap-3 text-pretty">
+          <div className="flex items-center gap-2">
+            <p
+              id={`report_date`}
+              className="my-1 min-w-24 text-right text-sm font-normal text-gray-500"
             >
-              <div className="flex items-center gap-2">
-                <p
-                  id={`report_date_${index}`}
-                  className="my-1 min-w-24 text-right text-sm font-normal text-gray-500"
-                >
-                  {randomDate.toDateString()}
-                </p>
-                <p
-                  id={`report_summary_${index}`}
-                  className="text-md my-1 font-medium text-gray-900"
-                >
-                  Report commentary {index}
-                </p>
-              </div>
-              <div>
-                <p
-                  id={`report_user_${index}`}
-                  className="my-1 w-fit rounded bg-gray-100 px-1 text-sm font-normal text-gray-500"
-                >
-                  Reported by user {index}
-                </p>
-              </div>
-            </div>
-          )
-        })}
+              {formattedDateTime}
+            </p>
+            <p
+              id={`report_summary`}
+              className="text-md my-1 font-medium text-gray-900"
+            >
+              {event_description}
+            </p>
+          </div>
+          <div>
+            <p
+              id={`report_user`}
+              className="my-1 w-fit rounded bg-gray-100 px-1 text-sm font-normal text-gray-500"
+            >
+              {last_updated_by}
+            </p>
+          </div>
+        </div>
+        {/*<hr className="h-px bg-gray-200 border-0 w-full"/>*/}
       </div>
     </div>
   )
