f96b4bd99bc565fa7138e7460f1a698dc2870f79
andreighh <61791875+andreighh@users.noreply.github.com>
2024-04-26 12:08:39 +0300

    Download uploaded files

--- frontend/src/components/manage/Blocks/Section/ReportInfo/FilesViewer.tsx
+++ frontend/src/components/manage/Blocks/Section/ReportInfo/FilesViewer.tsx
@@ -1,5 +1,7 @@
 import { ApiFile } from '@ors/types/api_files'
 
+import { formatApiUrl } from '@ors/helpers'
+
 import { IoDocumentTextOutline } from 'react-icons/io5'
 
 export function FilesViewer(props: { files?: ApiFile[]; heading: string }) {
@@ -17,7 +19,11 @@ export function FilesViewer(props: { files?: ApiFile[]; heading: string }) {
           </p>
         ) : (
           props.files.map((file, index) => (
-            <a key={index} className="m-0 flex items-center gap-2" href={file.download_url}>
+            <a
+              key={index}
+              className="m-0 flex w-min items-center gap-2"
+              href={formatApiUrl(file.download_url)}
+            >
               <IoDocumentTextOutline color="#0086C9" size="20" />
               <span className="text-lg text-gray-900">{file.filename}</span>
             </a>
