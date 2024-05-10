97c8aae42696bc06e3a66acf22f1d83fd7062d32
GeorgeFlorian <georgeflorian12@gmail.com>
2024-05-10 14:34:29 +0300

    Add empty export-data page

---
+++ frontend/src/app/country-programme/export-data/page.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+
+import React from 'react'
+
+import {Typography} from "@mui/material";
+
+import HeaderTitle from "@ors/components/theme/Header/HeaderTitle";
+import PageWrapper from '@ors/components/theme/PageWrapper/PageWrapper'
+
+export const metadata: Metadata = {
+  title: 'Export data',
+}
+
+export default async function ExportData() {
+  return (
+    <PageWrapper>
+      <HeaderTitle>
+        <Typography
+          className="text-typography-primary"
+          component="h1"
+          variant="h3"
+        >
+          Export data
+        </Typography>
+      </HeaderTitle>
+    </PageWrapper>
+  )
+}
