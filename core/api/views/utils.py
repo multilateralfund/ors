dead0f183d22bc2b32481223077fb4da21b01a3e
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2024-05-13 12:59:13 +0300

    Fixing large number of queries to User
    refs #24811

--- core/api/views/utils.py
+++ core/api/views/utils.py
@@ -17,7 +17,7 @@ def get_cp_report_from_request(request, cp_report_class):
 
     try:
         user = request.user
-        cp_report_qs = cp_report_class.objects.all()
+        cp_report_qs = cp_report_class.objects.select_related("user", "user__country")
         if user.user_type == user.UserType.COUNTRY_USER:
             cp_report_qs = cp_report_qs.filter(country=user.country)
 
