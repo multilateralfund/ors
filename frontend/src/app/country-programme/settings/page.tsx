b7b6e3a4e97e2d05a90371730f13163bed334015
Sloata George <39895513+GeorgeFlorian@users.noreply.github.com>
2024-05-13 16:46:38 +0300

    Add settings page; Add send email setting checkbox (#243)
    * Add initial CPSettings form
    
    * Add layout to settings page
    
    * Modify Common.Settings slice; Handle send_email checkbox
    
    * Add punctuation to labels

--- frontend/src/app/country-programme/settings/page.tsx
+++ frontend/src/app/country-programme/settings/page.tsx
@@ -2,27 +2,31 @@ import type { Metadata } from 'next'
 
 import React from 'react'
 
-import {Typography} from "@mui/material";
+import { Typography } from '@mui/material'
 
-import HeaderTitle from "@ors/components/theme/Header/HeaderTitle";
+import CPSettings from '@ors/components/manage/Blocks/CountryProgramme/CPSettings'
+import HeaderTitle from '@ors/components/theme/Header/HeaderTitle'
 import PageWrapper from '@ors/components/theme/PageWrapper/PageWrapper'
 
 export const metadata: Metadata = {
   title: 'Settings',
 }
 
-export default async function CPSettings() {
+export default async function CPSettingsWrapper() {
   return (
-    <PageWrapper>
+    <PageWrapper className="mx-auto max-w-screen-xl">
       <HeaderTitle>
-        <Typography
-          className="text-typography-primary"
-          component="h1"
-          variant="h3"
-        >
-          Settings
-        </Typography>
+        <div className="container mx-auto max-w-screen-xl">
+          <Typography
+            className="text-typography-primary"
+            component="h1"
+            variant="h3"
+          >
+            Settings
+          </Typography>
+        </div>
       </HeaderTitle>
+      <CPSettings />
     </PageWrapper>
   )
 }
