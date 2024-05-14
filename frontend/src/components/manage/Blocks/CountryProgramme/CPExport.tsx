b195d2cd3d7abffc359ce4ea44cb021e1f45248c
GeorgeFlorian <georgeflorian12@gmail.com>
2024-05-14 17:56:06 +0300

    Switch to <Field />; Add various improvements

--- frontend/src/components/manage/Blocks/CountryProgramme/CPExport.tsx
+++ frontend/src/components/manage/Blocks/CountryProgramme/CPExport.tsx
@@ -11,105 +11,107 @@ import {
   Typography,
 } from '@mui/material'
 
+import Field from '@ors/components/manage/Form/Field'
 import { formatApiUrl } from '@ors/helpers'
 import { useStore } from '@ors/store'
 
 import { IoInformationCircleOutline } from 'react-icons/io5'
 
+const updateDownloadUrl = (
+  exportOption: string,
+  year?: null | number,
+  minYear?: null | number,
+  maxYear?: null | number,
+) => {
+  let apiUrl = ''
+
+  if (minYear && maxYear && minYear > maxYear) {
+    return null
+  }
+
+  const yearParam = year === null ? '' : `year=${year}`
+  const minYearParam = minYear ? `min_year=${minYear}` : ''
+  const maxYearParam = maxYear ? `max_year=${maxYear}` : ''
+
+  switch (exportOption) {
+    case 'CP data extraction - all':
+      apiUrl = '/api/country-programme/data-extraction-all/export/'
+      break
+    case 'CP data extraction - HCFC':
+    case 'CP data extraction - HFC':
+      apiUrl = exportOption.includes('HCFC')
+        ? '/api/country-programme/hcfc/export/'
+        : '/api/country-programme/hfc/export/'
+      break
+    default:
+      return
+  }
+
+  let queryParams = exportOption.includes('all')
+    ? yearParam
+    : minYear && maxYear
+      ? `${minYearParam}&${maxYearParam}`
+      : `${minYearParam}` || `${maxYearParam}` || ''
+
+  queryParams = queryParams ? `?${queryParams}` : ''
+
+  return apiUrl + queryParams
+}
+
 const CPExport = () => {
   const settings = useStore((state) => state.common.settings.data)
 
   const [exportOption, setExportOption] = React.useState('')
-  const [year, setYear] = React.useState('currentYear')
-  const [minYear, setMinYear] = React.useState('')
-  const [maxYear, setMaxYear] = React.useState('')
+  const [year, setYear] = React.useState<null | number>(null)
+  const [minYear, setMinYear] = React.useState<null | number>(null)
+  const [maxYear, setMaxYear] = React.useState<null | number>(null)
 
-  const updateDownloadUrl = (
-    year?: string,
-    minYear?: string,
-    maxYear?: string,
-  ) => {
-    let apiUrl = ''
+  const calculatedMinYear = settings.cp_reports.min_year
+  const calculatedMaxYear = settings.cp_reports.max_year
 
-    if (minYear && maxYear && parseInt(minYear) > parseInt(maxYear)) {
-      return null
-    }
-
-    const yearParam = year === 'currentYear' ? '' : `?year=${year}`
-    const minYearParam = minYear ? `min_year=${parseInt(minYear)}` : ''
-    const maxYearParam = maxYear ? `max_year=${parseInt(maxYear)}` : ''
-
-    switch (exportOption) {
-      case 'CP data extraction - all':
-        apiUrl = '/api/country-programme/data-extraction-all/export/'
-        break
-      case 'CP data extraction - HCFC':
-      case 'CP data extraction - HFC':
-        apiUrl = exportOption.includes('HCFC')
-          ? '/api/country-programme/hcfc/export/'
-          : '/api/country-programme/hfc/export/'
-        break
-      default:
-        return
-    }
-
-    const queryParams = exportOption.includes('all')
-      ? yearParam
-      : minYear && maxYear
-        ? `?${minYearParam}&${maxYearParam}`
-        : `?${minYearParam}` || `?${maxYearParam}` || ''
-
-    return apiUrl + queryParams
-  }
-
-  const downloadUrl = updateDownloadUrl(year, minYear, maxYear)
+  const downloadUrl = updateDownloadUrl(exportOption, year, minYear, maxYear)
 
   const handleExportOptionChange = (event: SelectChangeEvent<string>) => {
     const selectedOption = event.target.value
     setExportOption(selectedOption)
 
     if (selectedOption.includes('all')) {
-      setYear('currentYear')
+      setYear(null)
     } else if (
       selectedOption.includes('HCFC') ||
       selectedOption.includes('HFC')
     ) {
-      setMinYear('')
-      setMaxYear('')
+      setMinYear(null)
+      setMaxYear(null)
     }
   }
 
-  const handleYearChange = (event: SelectChangeEvent<string>) => {
-    setYear(event.target.value)
-  }
-
-  const handleMinYearChange = (event: SelectChangeEvent<string>) => {
-    setMinYear(event.target.value)
-  }
-
-  const handleMaxYearChange = (event: SelectChangeEvent<string>) => {
-    setMaxYear(event.target.value)
-  }
-
-  // Create year options from settings.cp_reports.min_year to settings.cp_reports.max_year
   const yearOptions = []
   for (
     let i = settings.cp_reports.min_year;
     i <= settings.cp_reports.max_year;
     i++
   ) {
-    yearOptions.push(i.toString()) // Ensure it's a string
+    yearOptions.push({ id: i, label: i.toString(), value: i })
   }
 
+  const minYearOptions = yearOptions.filter(
+    (year) => year.value < (maxYear || calculatedMaxYear),
+  )
+
+  const maxYearOptions = yearOptions.filter(
+    (year) => year.value > (minYear || calculatedMinYear),
+  )
+
   return (
-    <div className="flex flex-col gap-4">
+    <div className="CPExport flex flex-col gap-4">
       <Alert icon={<IoInformationCircleOutline size={24} />} severity="info">
         <Typography variant="h6">
           Choose from the available types of extractions from the list below.
         </Typography>
       </Alert>
 
-      <div className="flex flex-wrap items-center justify-start gap-4">
+      <div className="flex flex-col flex-wrap items-end justify-start gap-4 md:flex-row">
         {/* Export options */}
         <FormControl className="w-1/3 min-w-52">
           <label htmlFor="export-option">Select an export option</label>
@@ -134,73 +136,88 @@ const CPExport = () => {
 
         {/* Year Select */}
         {exportOption && exportOption.includes('all') && (
-          <FormControl className="w-1/3 min-w-28">
+          <div className="flex w-1/3 min-w-28 flex-col">
             <label htmlFor="year">Year</label>
-            <Select
+            <Field
               id="year"
-              placeholder="Select a year"
-              size="small"
-              value={year}
-              MenuProps={{
-                sx: { maxHeight: 400 },
+              FieldProps={{ className: 'mb-0' }}
+              options={yearOptions}
+              widget="autocomplete"
+              Input={{
+                placeholder: 'Select year...',
               }}
-              onChange={handleYearChange}
-            >
-              <MenuItem key={'currentYear'} value={'currentYear'}>
-                Current Year
-              </MenuItem>
-              {yearOptions.map((option) => (
-                <MenuItem key={option} value={option}>
-                  {option}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+              isOptionEqualToValue={(option, value) =>
+                option?.value === value?.value
+              }
+              value={
+                year ? { id: year, label: year?.toString(), value: year } : null
+              }
+              onChange={(_, option: any) => {
+                setYear(option?.value || null)
+              }}
+            />
+          </div>
         )}
 
         {/* Min/Max year select */}
         {exportOption &&
           (exportOption.includes('HCFC') || exportOption.includes('HFC')) && (
             <>
-              <FormControl className="w-1/4 min-w-28">
+              <div className="flex w-1/4 min-w-28 flex-col">
                 <label htmlFor="min-year">Min Year</label>
-                <Select
-                  id="min-year"
-                  placeholder="Select a min year"
-                  size="small"
-                  value={minYear}
-                  MenuProps={{
-                    sx: { maxHeight: 400 },
-                  }}
-                  onChange={handleMinYearChange}
-                >
-                  {yearOptions.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
 
-              <FormControl className="w-1/4 min-w-28">
-                <label htmlFor="max-year">Max Year</label>
-                <Select
-                  id="max-year"
-                  placeholder="Select a max year"
-                  size="small"
-                  value={maxYear}
-                  MenuProps={{
-                    sx: { maxHeight: 400 },
+                <Field
+                  id="min-year"
+                  FieldProps={{ className: 'mb-0' }}
+                  options={minYearOptions}
+                  widget="autocomplete"
+                  Input={{
+                    placeholder: 'Select min year...',
                   }}
-                  onChange={handleMaxYearChange}
-                >
-                  {yearOptions.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
+                  isOptionEqualToValue={(option, value) =>
+                    option?.value === value?.value
+                  }
+                  value={
+                    minYear
+                      ? {
+                          id: minYear,
+                          label: minYear?.toString(),
+                          value: minYear,
+                        }
+                      : null
+                  }
+                  onChange={(_, option: any) => {
+                    setMinYear(option?.value || null)
+                  }}
+                />
+              </div>
+              <div className="flex w-1/4 min-w-28 flex-col">
+                <label htmlFor="max-year">Max Year</label>
+                <Field
+                  id="max-year"
+                  FieldProps={{ className: 'mb-0' }}
+                  options={maxYearOptions}
+                  widget="autocomplete"
+                  Input={{
+                    placeholder: 'Select max year...',
+                  }}
+                  isOptionEqualToValue={(option, value) =>
+                    option?.value === value?.value
+                  }
+                  value={
+                    maxYear
+                      ? {
+                          id: maxYear,
+                          label: maxYear?.toString(),
+                          value: maxYear,
+                        }
+                      : null
+                  }
+                  onChange={(_, option: any) => {
+                    setMaxYear(option?.value || null)
+                  }}
+                />
+              </div>
             </>
           )}
 
