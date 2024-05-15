86f13e40dc002b39308b78d1f25b914f5efcc660
GeorgeFlorian <georgeflorian12@gmail.com>
2024-05-15 19:12:01 +0300

    Add See Diff and Go Back buttons

--- frontend/src/components/manage/Blocks/CountryProgramme/CPHeader.tsx
+++ frontend/src/components/manage/Blocks/CountryProgramme/CPHeader.tsx
@@ -6,10 +6,11 @@ import { Button } from '@mui/material'
 import cx from 'classnames'
 import { Dictionary, capitalize, orderBy } from 'lodash'
 import NextLink from 'next/link'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { useSnackbar } from 'notistack'
 
 import HeaderTitle from '@ors/components/theme/Header/HeaderTitle'
+import Loading from '@ors/components/theme/Loading/Loading'
 import Link from '@ors/components/ui/Link/Link'
 import { uploadFiles } from '@ors/helpers'
 import api from '@ors/helpers/Api/_api'
@@ -17,6 +18,57 @@ import useClickOutside from '@ors/hooks/useClickOutside'
 import { useStore } from '@ors/store'
 
 import { IoChevronDown } from 'react-icons/io5'
+
+const CloseDiffButton = () => {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  if (!pathname.includes('diff')) return null
+
+  return (
+    <Button
+      color="secondary"
+      // href="/country-programme"
+      size="large"
+      variant="contained"
+      onClick={() => {
+        router.back()
+      }}
+    >
+      Go back
+    </Button>
+  )
+}
+
+const ReportDiffButton = () => {
+  const { report } = useStore((state) => state.cp_reports)
+  const pathname = usePathname()
+  const countries = useStore((state) => {
+    return state.common.countries_for_listing.data
+  })
+
+  if (!report) return <Loading />
+  if (pathname.includes('diff')) return null
+
+  const countriesById = new Map<number, any>(
+    countries.map((country: any) => [country.id, country]),
+  )
+
+  const { country_id, year } = report.data || {}
+  const iso3 = countriesById.get(country_id || 0)?.iso3
+
+  return (
+    <Link
+      color="secondary"
+      href={`/country-programme/${iso3}/${year}/diff`}
+      size="large"
+      variant="contained"
+      button
+    >
+      See difference
+    </Link>
+  )
+}
 
 function padDateNr(n: number) {
   return n < 10 ? `0${n}` : `${n}`
@@ -36,6 +88,8 @@ const HeaderVersionsDropdown = () => {
   const ref = useClickOutside<HTMLDivElement>(() => {
     setShowVersionsMenu(false)
   })
+  const pathname = usePathname()
+  const onDiffPage = pathname.includes('diff')
 
   const versions =
     report.data && report.country
@@ -68,39 +122,45 @@ const HeaderVersionsDropdown = () => {
   return (
     <div className="relative">
       <div
-        className="flex cursor-pointer items-center justify-between gap-x-2"
+        className={cx('flex items-center justify-between gap-x-2', {
+          'cursor-pointer': !onDiffPage,
+        })}
         ref={ref}
         onClick={toggleShowVersionsMenu}
       >
         <h1 className="m-0 text-5xl leading-normal">{report?.data?.name}</h1>
-        <IoChevronDown className="text-5xl font-bold text-gray-700" />
-      </div>
-      <div
-        className={cx(
-          'absolute left-0 z-10 max-h-[200px] origin-top overflow-y-auto rounded-none border border-solid border-primary bg-gray-A100 opacity-0 transition-all',
-          {
-            'collapse scale-y-0': !showVersionsMenu,
-            'scale-y-100 opacity-100': showVersionsMenu,
-          },
+        {!onDiffPage && (
+          <IoChevronDown className="text-5xl font-bold text-gray-700" />
         )}
-      >
-        {versions.map((info, idx) => (
-          <NextLink
-            key={info.id}
-            className="flex items-center gap-x-2 rounded-none px-2 py-2 text-black no-underline hover:bg-primary hover:text-white"
-            href={info.url}
-          >
-            <div className="flex w-56 items-center justify-between hover:text-white">
-              <div>{info.label}</div>
-              <div className="flex items-center">
-                {idx == 0 && (info.isFinal ? tagLatest : tagDraft)}
-                {idx == 1 && versions[0].isDraft && tagLatest}
-                {info.formattedDate}
-              </div>
-            </div>
-          </NextLink>
-        ))}
       </div>
+      {!onDiffPage && (
+        <div
+          className={cx(
+            'absolute left-0 z-10 max-h-[200px] origin-top overflow-y-auto rounded-none border border-solid border-primary bg-gray-A100 opacity-0 transition-all',
+            {
+              'collapse scale-y-0': !showVersionsMenu,
+              'scale-y-100 opacity-100': showVersionsMenu,
+            },
+          )}
+        >
+          {versions.map((info, idx) => (
+            <NextLink
+              key={info.id}
+              className="flex items-center gap-x-2 rounded-none px-2 py-2 text-black no-underline hover:bg-primary hover:text-white"
+              href={info.url}
+            >
+              <div className="flex w-56 items-center justify-between hover:text-white">
+                <div>{info.label}</div>
+                <div className="flex items-center">
+                  {idx == 0 && (info.isFinal ? tagLatest : tagDraft)}
+                  {idx == 1 && versions[0].isDraft && tagLatest}
+                  {info.formattedDate}
+                </div>
+              </div>
+            </NextLink>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
@@ -167,7 +227,7 @@ const ArchiveHeaderTag = () => {
 }
 
 const ArchiveHeaderActions = () => {
-  return <div className="flex items-center"></div>
+  return null
 }
 
 const ViewHeaderActions = () => {
@@ -180,6 +240,8 @@ const ViewHeaderActions = () => {
   const isDraft = report.data?.status === 'draft'
 
   if (!userTypeVisibility[user_type as UserType]) return null
+
+  console.log(report.data)
 
   return (
     <div className="flex items-center">
@@ -279,7 +341,7 @@ const EditHeaderActions = ({
         if (files && files.length > 0) {
           await uploadFiles(
             `api/country-programme/files/?country_id=${report.country?.id}&year=${report.data?.year}`,
-            files
+            files,
           )
         }
 
@@ -405,8 +467,10 @@ const CPHeader = ({
             {titlePrefix}
             <HeaderVersionsDropdown />
             {tag}
+            <ReportDiffButton />
           </div>
           {actions}
+          <CloseDiffButton />
         </div>
       </HeaderTitle>
     )
