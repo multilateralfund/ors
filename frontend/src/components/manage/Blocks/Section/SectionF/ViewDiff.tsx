ef9f8d8cd81819d77564fda426f0a0807699b353
GeorgeFlorian <georgeflorian12@gmail.com>
2024-05-22 13:54:44 +0300

    Improve SectionF Diff

--- frontend/src/components/manage/Blocks/Section/SectionF/ViewDiff.tsx
+++ frontend/src/components/manage/Blocks/Section/SectionF/ViewDiff.tsx
@@ -1,11 +1,11 @@
 import { Box, Typography } from '@mui/material'
 
-import Field from '@ors/components/manage/Form/Field'
-
 export default function SectionFViewDiff(props: any) {
   const { report, section } = props
 
-  const newRemakrs = report.section_f?.[0].remarks
+  if (!report.section_f.length) return null
+
+  const newRemarks = report.section_f?.[0].remarks
   const oldRemarks = report.section_f?.[0].remarks_old
 
   return (
@@ -14,19 +14,22 @@ export default function SectionFViewDiff(props: any) {
         <Typography className="mb-4" component="h2" variant="h6">
           {section.title}
         </Typography>
-        <Field
-          FieldProps={{ className: 'mb-0' }}
-          readOnly={true}
-          type="textarea"
-          value={newRemakrs}
-        />
-        <Field
-          FieldProps={{ className: 'mb-0' }}
-          readOnly={true}
-          type="textarea"
-          value={oldRemarks}
-        />
-
+        <div className="mb-4">
+          <Typography component="h4" variant="h4">
+            New Remarks
+          </Typography>
+          <Typography className="text-lg text-gray-900">
+            {newRemarks}
+          </Typography>
+        </div>
+        {oldRemarks && (
+          <div className="text-gray-400">
+            <Typography component="h4" variant="h4">
+              Old Remarks
+            </Typography>
+            <Typography className="text-lg">{oldRemarks}</Typography>
+          </div>
+        )}
       </Box>
     </>
   )
