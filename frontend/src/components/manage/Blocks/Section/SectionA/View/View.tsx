832d7a4f8537a63f379d1cde9ea559a2ffaca23e
David Batranu <david.batranu@eaudeweb.ro>
2024-05-27 22:58:08 +0300

    Add bottom row, add header components.

--- frontend/src/components/manage/Blocks/Section/SectionA/View/View.tsx
+++ frontend/src/components/manage/Blocks/Section/SectionA/View/View.tsx
@@ -152,9 +152,9 @@ export default function SectionAView(props: SectionAViewProps) {
       </div>
       <SimpleTable
         {...TableProps}
-        columnDefs={gridOptionsAll.columnDefs}
-        defaultColDef={gridOptionsAll.defaultColDef}
-        rowData={rowData}
+        columnDefs={gridOptions.columnDefs}
+        defaultColDef={gridOptions.defaultColDef}
+        rowData={[...rowData, ...pinnedBottomRowData]}
       />
       <Table
         key={tableDataValue}
