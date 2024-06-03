acd8b24145e0ef877708c1e851baa2487e7c91eb
Rares Siteavu <89769498+raressiteavu@users.noreply.github.com>
2024-06-03 14:44:41 +0300

    BP send mail events (#277)

--- core/api/tests/test_bp_comments.py
+++ core/api/tests/test_bp_comments.py
@@ -1,8 +1,16 @@
 import pytest
+from constance import config
 from django.urls import reverse
 from rest_framework.test import APIClient
+from unittest.mock import patch
 
 pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture(name="mock_send_mail_comment")
+def _mock_send_mail_comment():
+    with patch("core.tasks.send_mail_comment_submit_bp.delay") as send_mail:
+        yield send_mail
 
 
 class TestBPComments:
@@ -34,7 +42,9 @@ class TestBPComments:
         response = self.client.post(url, data, format="json")
         assert response.status_code == 400
 
-    def test_create_comments(self, user, agency_user, business_plan):
+    def test_create_comments(
+        self, user, agency_user, business_plan, mock_send_mail_comment
+    ):
         url = reverse("business-plan-comments", kwargs={"id": business_plan.id})
 
         self.client.force_authenticate(user=agency_user)
@@ -71,3 +81,23 @@ class TestBPComments:
         assert response.status_code == 200
         assert response.data["comment_agency"] == "Test create agency"
         assert response.data["comment_secretariat"] == "Test update secretariat"
+
+        # check 3 emails sent (3 comments)
+        mock_send_mail_comment.assert_called()
+        assert mock_send_mail_comment.call_count == 3
+
+    def test_config_mail_not_sent(self, user, business_plan, mock_send_mail_comment):
+        config.SEND_MAIL = False  # change config
+        url = reverse("business-plan-comments", kwargs={"id": business_plan.id})
+
+        self.client.force_authenticate(user=user)
+        # create  comment
+        data = {
+            "comment_type": self.COMMENT_SECRETARIAT,
+            "comment": "Test config - mail not sent",
+        }
+        response = self.client.post(url, data, format="json")
+        assert response.status_code == 201
+
+        # check email not sent
+        mock_send_mail_comment.assert_not_called()
