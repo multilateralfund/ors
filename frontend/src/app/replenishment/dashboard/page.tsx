82dbf63cb7692f1341bd5083c28c893d17895f38
David Batranu <david.batranu@eaudeweb.ro>
2024-06-04 14:38:06 +0300

    Dashboard placeholder data.

--- frontend/src/app/replenishment/dashboard/page.tsx
+++ frontend/src/app/replenishment/dashboard/page.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 import { Typography } from '@mui/material'
 
+import DashboardView from '@ors/components/manage/Blocks/Replenishment/DashboardView'
 import HeaderTitle from '@ors/components/theme/Header/HeaderTitle'
 import PageWrapper from '@ors/components/theme/PageWrapper/PageWrapper'
 
@@ -13,7 +14,7 @@ export const metadata: Metadata = {
 
 export default async function ReplenishmentDashboard() {
   return (
-    <PageWrapper className="max-w-screen-xl lg:px-0">
+    <PageWrapper className="w-full p-2" defaultSpacing={false}>
       <HeaderTitle>
         <Typography
           className="text-typography-primary"
@@ -23,6 +24,7 @@ export default async function ReplenishmentDashboard() {
           Replenishment - Dashboard
         </Typography>
       </HeaderTitle>
+      <DashboardView />
     </PageWrapper>
   )
 }
