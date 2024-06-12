1cd06ca8547b4a27a7e07dfcc08cae4309fec121
andreighh <61791875+andreighh@users.noreply.github.com>
2024-06-12 13:44:47 +0300

    Redo models and remake import

--- core/models/replenishment.py
+++ core/models/replenishment.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.functional import cached_property
 
 from core.models import Country
 from core.models.utils import get_protected_storage
@@ -8,7 +7,7 @@ from core.models.utils import get_protected_storage
 class Replenishment(models.Model):
     start_year = models.IntegerField()
     end_year = models.IntegerField()
-    amount = models.DecimalField(max_digits=20, decimal_places=6)
+    amount = models.DecimalField(max_digits=20, decimal_places=5)
 
     def __str__(self):
         return f"Replenishment ({self.start_year} - {self.end_year})"
@@ -17,35 +16,40 @@ class Replenishment(models.Model):
 class Contribution(models.Model):
     replenishment = models.ForeignKey(
         Replenishment,
-        on_delete=models.SET_NULL,
+        on_delete=models.PROTECT,
         null=True,
         related_name="contributions",
     )
     country = models.ForeignKey(
-        Country, on_delete=models.SET_NULL, null=True, related_name="contributions"
+        Country, on_delete=models.PROTECT, null=True, related_name="contributions"
     )
     paid_in_local_currency = models.BooleanField(default=False)
-    amount = models.DecimalField(max_digits=20, decimal_places=6)
+    amount = models.DecimalField(max_digits=20, decimal_places=5)
     currency_of_payment = models.CharField(max_length=16)
     exchange_rate_six_months_prior = models.DecimalField(
-        max_digits=20, decimal_places=6
+        max_digits=20, decimal_places=5
     )
     bilateral_assistance_amount = models.DecimalField(
-        max_digits=20, decimal_places=6, default=0
+        max_digits=20, decimal_places=5, default=0
     )
-    un_scale_of_assessment = models.DecimalField(max_digits=20, decimal_places=6)
-    overridden_scale_of_assessment = models.DecimalField(
-        max_digits=20, decimal_places=6, null=True
+    un_scale_of_assessment = models.DecimalField(max_digits=20, decimal_places=5)
+    edited_scale_of_assessment = models.DecimalField(
+        max_digits=20, decimal_places=5, null=True
     )
     average_contributor_inflation_rate = models.DecimalField(
-        max_digits=20, decimal_places=6
+        max_digits=20, decimal_places=5
     )
-    qualifies_for_fixed_rate_mechanism = models.BooleanField(default=False)
+    edited_qualifies_for_fixed_rate_mechanism = models.BooleanField(default=False)
 
-    @cached_property
+    @property
     def adjusted_scale_of_assessment(self):
         # TODO: formula
-        return self.overridden_scale_of_assessment or 0
+        return self.edited_scale_of_assessment or 0
+
+    @property
+    def qualifies_for_fixed_rate_mechanism(self):
+        # TODO: formula
+        return self.edited_qualifies_for_fixed_rate_mechanism or 0
 
     def __str__(self):
         return f"Contribution {self.country.name} ({self.replenishment.start_year} - {self.replenishment.end_year})"
@@ -53,11 +57,12 @@ class Contribution(models.Model):
 
 class Invoice(models.Model):
     country = models.ForeignKey(
-        Country, on_delete=models.SET_NULL, null=True, related_name="invoices"
+        Country, on_delete=models.PROTECT, null=True, related_name="invoices"
     )
     replenishment = models.ForeignKey(
-        Replenishment, on_delete=models.SET_NULL, null=True, related_name="invoices"
+        Replenishment, on_delete=models.PROTECT, null=True, related_name="invoices"
     )
+    amount = models.DecimalField(max_digits=20, decimal_places=5)
     date = models.DateField()
     number = models.CharField(max_length=128, unique=True)
 
@@ -66,6 +71,9 @@ class Invoice(models.Model):
 
 
 class InvoiceFile(models.Model):
+    class InvoiceFileType(models.TextChoices):
+        INVOICE = "Invoice", "Invoice"
+
     def upload_path(self, filename):
         return f"invoice_files/{self.invoice.country}/{self.invoice.number}__{filename}"
 
@@ -77,26 +85,33 @@ class InvoiceFile(models.Model):
     )
     filename = models.CharField(max_length=128)
     file = models.FileField(storage=get_protected_storage, upload_to=upload_path)
+    file_type = models.CharField(
+        max_length=16, choices=InvoiceFileType.choices, default=InvoiceFileType.INVOICE
+    )
 
 
 class Payment(models.Model):
     country = models.ForeignKey(
-        Country, on_delete=models.SET_NULL, null=True, related_name="payments"
+        Country, on_delete=models.PROTECT, null=True, related_name="payments"
     )
     replenishment = models.ForeignKey(
-        Replenishment, on_delete=models.SET_NULL, null=True, related_name="payments"
+        Replenishment, on_delete=models.PROTECT, null=True, related_name="payments"
     )
     date = models.DateField()
     payment_for_year = models.CharField(max_length=16)
-    gain_or_loss = models.DecimalField(max_digits=20, decimal_places=6)
-    amount_local_currency = models.DecimalField(max_digits=20, decimal_places=6)
-    amount_usd = models.DecimalField(max_digits=20, decimal_places=6)
+    gain_or_loss = models.DecimalField(max_digits=20, decimal_places=5)
+    amount_local_currency = models.DecimalField(max_digits=20, decimal_places=5)
+    amount_usd = models.DecimalField(max_digits=20, decimal_places=5)
 
     def __str__(self):
         return f"Payment {self.country.name} - {self.payment_for_year}"
 
 
 class PaymentFile(models.Model):
+    class PaymentFileType(models.TextChoices):
+        BANK_STATEMENT = "Bank Statement", "Bank Statement"
+        PROMISSORY_NOTE = "Promissory Note", "Promissory Note"
+
     def upload_path(self, filename):
         return f"payment_files/{self.payment.country}/{self.payment.id}__{filename}"
 
@@ -108,17 +123,8 @@ class PaymentFile(models.Model):
     )
     filename = models.CharField(max_length=128)
     file = models.FileField(storage=get_protected_storage, upload_to=upload_path)
-
-
-class PromissoryNoteFile(models.Model):
-    def upload_path(self, filename):
-        return f"promissory_note_files/{self.payment.country}/{self.payment.id}__{filename}"
-
-    uploaded_at = models.DateTimeField(
-        auto_now_add=True, help_text="Date of promissory note file upload"
+    file_type = models.CharField(
+        max_length=32,
+        choices=PaymentFileType.choices,
+        default=PaymentFileType.BANK_STATEMENT,
     )
-    payment = models.ForeignKey(
-        Payment, on_delete=models.CASCADE, related_name="promissory_note_files"
-    )
-    filename = models.CharField(max_length=128)
-    file = models.FileField(storage=get_protected_storage, upload_to=upload_path)
