22e3730e2df0d7c8255a78663dbd7e0012e8ba97
andreighh <61791875+andreighh@users.noreply.github.com>
2024-06-25 16:19:44 +0300

    Grey out disabled inputs

--- frontend/src/components/manage/Blocks/Section/ReportInfo/SimpleInput.tsx
+++ frontend/src/components/manage/Blocks/Section/ReportInfo/SimpleInput.tsx
@@ -26,7 +26,7 @@ const SimpleInput = ({
       <input
         id={id}
         name={id}
-        className="text-md block h-10 w-full rounded-lg border border-solid border-gray-400 bg-white p-2.5 text-gray-900 shadow-none focus:border-blue-500 focus:ring-blue-500"
+        className="text-md block h-10 w-full rounded-lg border border-solid border-gray-400 bg-white p-2.5 text-gray-900 shadow-none focus:border-blue-500 focus:ring-blue-500 disabled:border-gray-400 disabled:bg-gray-200 disabled:text-gray-500"
         autoComplete="off"
         defaultValue={defaultValue}
         disabled={disabled}
