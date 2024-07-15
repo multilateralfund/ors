53abd15466783e8741cd624d5b1214b8420c5827
GeorgeFlorian <georgeflorian12@gmail.com>
2024-07-15 16:53:19 +0300

    Add Indicators for SoC Triennial

--- frontend/src/components/manage/Blocks/Replenishment/StatusOfContribution/Indicators.jsx
+++ frontend/src/components/manage/Blocks/Replenishment/StatusOfContribution/Indicators.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function IndicatorBox({ text, value }) {
+function IndicatorBox({ isPercentage, text, value }) {
   return (
-    <div className="flex min-w-80 max-w-[360px] flex-1 items-center justify-center gap-5 rounded-lg border-2 border-solid border-gray-200 p-4 uppercase text-primary">
-      <span className="text-6xl font-bold">{value}</span>
+    <div className="flex min-w-80 max-w-96 flex-1 items-center justify-center gap-5 rounded-lg border-2 border-solid border-gray-200 p-4 uppercase text-primary">
+      <span className="text-6xl font-bold">
+        {value}
+        {isPercentage && '%'}
+      </span>
       <span className="text-2xl font-medium">{text}</span>
     </div>
   )
@@ -28,4 +31,20 @@ const SummaryIndicators = ({ data }) => {
   )
 }
 
-export { SummaryIndicators }
+const TriennialIndicators = ({ data, totalPledge }) => {
+  return (
+    <div className="flex flex-wrap items-center justify-center gap-4 border-primary text-primary">
+      <IndicatorBox
+        text="parties have made their contributions"
+        value={data.contributions}
+      />
+      <IndicatorBox
+        text="of the total pledge received for the triennial"
+        value={totalPledge}
+        isPercentage
+      />
+    </div>
+  )
+}
+
+export { SummaryIndicators, TriennialIndicators }
