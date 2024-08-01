6c10299886d8df8fa9a8b07cd7c45c85f5f00e42
GeorgeFlorian <georgeflorian12@gmail.com>
2024-08-01 14:11:40 +0300

    Add pagination to BPList activities tab; Link filters to activities fetcher

--- frontend/src/components/manage/Blocks/BusinessPlans/BPList/BPActivities.tsx
+++ frontend/src/components/manage/Blocks/BusinessPlans/BPList/BPActivities.tsx
@@ -4,27 +4,41 @@ import { useRef, useState } from 'react'
 
 import Activities from '@ors/components/manage/Blocks/BusinessPlans/Activities'
 import ActivitiesFilters from '@ors/components/manage/Blocks/BusinessPlans/ActivitiesFilters'
+import useGetBpPeriods from '@ors/components/manage/Blocks/BusinessPlans/BPList/useGetBPPeriods'
 import { useGetActivities } from '@ors/components/manage/Blocks/BusinessPlans/useGetActivities'
+import { Pagination } from '@ors/components/ui/Pagination/Pagination'
 import { useStore } from '@ors/store'
+
+const ACTIVITIES_PER_PAGE = 20
 
 export default function BPActivities(props: any) {
   const { period } = props
+  const { periodOptions } = useGetBpPeriods()
+
+  const firstPeriod = periodOptions[periodOptions.length - 1].value
+  const lastPeriod = periodOptions[0].value
   const commonSlice = useStore((state) => state.common)
   const bpSlice = useStore((state) => state.businessPlans)
   const projects = useStore((state) => state.projects)
   const form = useRef<any>()
-
   const clusters = projects.clusters.data || []
 
-  // TODO: Add filters and link them to useGetActivities
+  const year_end = period?.split('-')[1] || lastPeriod.split('-')[1]
+  const year_start = period?.split('-')[0] || firstPeriod.split('-')[0]
+
   const initialFilters = {
-    // year_end: end_year,
-    year_start: period?.split('-')[0],
+    limit: ACTIVITIES_PER_PAGE,
+    offset: 0,
+    year_end: year_end,
+    year_start: year_start,
   }
 
   const [filters, setFilters] = useState({ ...initialFilters })
-  const { loaded, results, setParams } = useGetActivities(period)
-
+  const { count, loaded, results, setParams } = useGetActivities(initialFilters)
+  const [pagination, setPagination] = useState({
+    page: 1,
+    rowsPerPage: ACTIVITIES_PER_PAGE,
+  })
   function handleFilterChange(newFilters: { [key: string]: any }) {
     setFilters((filters) => ({ ...filters, ...newFilters }))
   }
@@ -33,20 +47,41 @@ export default function BPActivities(props: any) {
     setParams(params)
   }
 
-  // TODO: Add Pagination
+  // TODO: What are activities ordered by default ?
+
+  const pages = Math.ceil(count / pagination.rowsPerPage)
 
   return (
-    <form ref={form}>
-      <ActivitiesFilters
-        bpSlice={bpSlice}
-        clusters={clusters}
-        commonSlice={commonSlice}
-        filters={filters}
-        form={form}
-        handleFilterChange={handleFilterChange}
-        handleParamsChange={handleParamsChange}
-      />
-      <Activities loaded={loaded} period={period} results={results} />
-    </form>
+    <>
+      <form ref={form}>
+        <ActivitiesFilters
+          bpSlice={bpSlice}
+          clusters={clusters}
+          commonSlice={commonSlice}
+          filters={filters}
+          form={form}
+          handleFilterChange={handleFilterChange}
+          handleParamsChange={handleParamsChange}
+        />
+        <Activities loaded={loaded} period={period} results={results} />
+      </form>
+
+      {!!pages && pages > 1 && (
+        <div className="mt-4 flex items-center justify-start">
+          <Pagination
+            count={pages}
+            page={pagination.page}
+            siblingCount={1}
+            onPaginationChanged={(page) => {
+              setPagination({ ...pagination, page: page || 1 })
+              setParams({
+                limit: pagination.rowsPerPage,
+                offset: ((page || 1) - 1) * pagination.rowsPerPage,
+              })
+            }}
+          />
+        </div>
+      )}
+    </>
   )
 }
--- frontend/src/components/manage/Blocks/BusinessPlans/useGetActivities.ts
+++ frontend/src/components/manage/Blocks/BusinessPlans/useGetActivities.ts
@@ -1,31 +1,16 @@
-import useGetBpPeriods from '@ors/components/manage/Blocks/BusinessPlans/BPList/useGetBPPeriods'
 import { getResults } from '@ors/helpers'
 import useApi from '@ors/hooks/useApi'
 
-const BP_PER_PAGE = 20
-
-export function useGetActivities(period: string) {
-  const { periodOptions } = useGetBpPeriods()
-
-  const firstPeriod = periodOptions[periodOptions.length - 1].value
-  const lastPeriod = periodOptions[0].value
-
-  const year_end = period?.split('-')[1] || lastPeriod.split('-')[1]
-  const year_start = period?.split('-')[0] || firstPeriod.split('-')[0]
-
+export function useGetActivities(initialFilters: any) {
   const { data, loading, setParams } = useApi({
     options: {
-      params: {
-        limit: BP_PER_PAGE,
-        offset: 0,
-        year_end: year_end,
-        year_start: year_start,
-      },
+      params: { ...initialFilters },
       withStoreCache: false,
     },
+    // path: 'api/business-plan-activity/',
     path: 'api/business-plan-record/',
   })
   const { count, loaded, results } = getResults(data)
 
-  return { loaded, loading, results, setParams }
+  return { count, loaded, loading, results, setParams }
 }
