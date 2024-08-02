8ff39ed63fdbed8527391cb5d3726e55eeb7e64d
GeorgeFlorian <georgeflorian12@gmail.com>
2024-08-02 15:38:01 +0300

    Fixed bug where the CP Section overlay has a bigger z-index than the Navigation's Menu dropdown

--- frontend/src/components/ui/SectionOverlay/SectionOverlay.tsx
+++ frontend/src/components/ui/SectionOverlay/SectionOverlay.tsx
@@ -1,7 +1,7 @@
 const SectionOverlay = ({ className = "", opacity = 'opacity-70' }) => {
   return (
     <div
-      className={`absolute inset-0 z-50 bg-gray-100 ${opacity} ${className}`}
+      className={`absolute inset-0 z-40 bg-gray-100 ${opacity} ${className}`}
     ></div>
   )
 }
