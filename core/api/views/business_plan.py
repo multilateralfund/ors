cdd7d7a00c470250d6ff68409c064cef33361397
Rares Siteavu <rares.siteavu@yahoo.ro>
2024-09-23 15:09:07 +0300

    Update BP diff fields

--- core/api/views/business_plan.py
+++ core/api/views/business_plan.py
@@ -606,17 +606,28 @@ class BPActivityDiffView(mixins.ListModelMixin, generics.GenericAPIView):
     permission_classes = [IsSecretariat | IsAgency | IsViewer]
 
     fields = [
+        "country",
+        "project_cluster",
+        "project_type",
+        "bp_chemical_type",
+        "sector",
+        "subsector",
+        "substances",
+        "substances_display",
         "title",
         "required_by_model",
         "lvc_status",
         "amount_polyol",
         "legacy_sector_and_subsector",
         "status",
+        "status_display",
         "is_multi_year",
+        "is_multi_year_display",
         "reason_for_exceeding",
         "remarks",
         "remarks_additional",
         "comment_secretariat",
+        "comment_types",
     ]
 
     def diff_activities(self, data, data_old):
@@ -708,10 +719,10 @@ class BPActivityDiffView(mixins.ListModelMixin, generics.GenericAPIView):
 
         return Response(
             self.diff_activities(
-                BPActivityCreateSerializer(
+                BPActivityDetailSerializer(
                     business_plan.activities.all(), many=True
                 ).data,
-                BPActivityCreateSerializer(
+                BPActivityDetailSerializer(
                     business_plan_ar.activities.all(), many=True
                 ).data,
             ),
