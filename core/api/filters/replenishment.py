d638a0cf87d7cd99a23b7a980feba19ec871ac30
sorinstelian <sorin.stelian@eaudeweb.ro>
2024-09-25 17:25:14 +0300

    Adding more invoice filters (#419)
    * Adding reminders_sent filters
    
    * adding tests
    
    * Adding more replenishment filters
    
    * fixing linting errors
    
    * Disabling wrong pylint suggestion

--- core/api/filters/replenishment.py
+++ core/api/filters/replenishment.py
@@ -1,5 +1,6 @@
 from django_filters import rest_framework as filters
 from django_filters.widgets import CSVWidget
+from django.db.models import OuterRef, Subquery
 
 from core.models import Country, Invoice, Payment, ScaleOfAssessment
 
@@ -28,6 +29,9 @@ class InvoiceFilter(filters.FilterSet):
     year = filters.NumberFilter(field_name="year")
     status = filters.CharFilter(method="filter_status")
 
+    reminders_sent = filters.NumberFilter(method="filter_reminders_sent")
+    opted_for_ferm = filters.BooleanFilter(method="filter_opted_for_ferm")
+
     def filter_status(self, queryset, _name, value):
         if value == "pending":
             return queryset.filter(date_paid__isnull=True)
@@ -35,9 +39,40 @@ class InvoiceFilter(filters.FilterSet):
             return queryset.filter(date_paid__isnull=False)
         return queryset
 
+    def filter_reminders_sent(self, queryset, _name, value):
+        # pylint: disable-next=R1705
+        if value == 0:
+            return queryset.filter(
+                date_first_reminder__isnull=True, date_second_reminder__isnull=True
+            )
+        elif value == 1:
+            return queryset.filter(
+                date_first_reminder__isnull=False, date_second_reminder__isnull=True
+            )
+        elif value == 2:
+            return queryset.filter(
+                date_first_reminder__isnull=False, date_second_reminder__isnull=False
+            )
+
+        return queryset
+
+    def filter_opted_for_ferm(self, queryset, _name, value):
+        # `queryset` is of Invoice
+        ret = queryset.annotate(
+            opted_for_ferm=Subquery(
+                ScaleOfAssessment.objects.filter(
+                    country=OuterRef("country"),
+                    version__replenishment__start_year__lte=OuterRef("year"),
+                    version__replenishment__end_year__gte=OuterRef("year"),
+                ).values("opted_for_ferm")[:1]
+            )
+        ).filter(opted_for_ferm=value)
+
+        return ret
+
     class Meta:
         model = Invoice
-        fields = ["country_id", "year", "status"]
+        fields = ["country_id", "year", "status", "reminders_sent"]
 
 
 class PaymentFilter(filters.FilterSet):
