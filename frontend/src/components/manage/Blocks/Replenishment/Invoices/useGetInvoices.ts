5845ae9106f18b17746ffb1e7297f6e1ab86e3c0
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2024-09-26 17:42:01 +0300

    perfectionism

--- frontend/src/components/manage/Blocks/Replenishment/Invoices/useGetInvoices.ts
+++ frontend/src/components/manage/Blocks/Replenishment/Invoices/useGetInvoices.ts
@@ -2,15 +2,15 @@ import { ApiReplenishmentInvoices } from '@ors/types/api_replenishment_invoices'
 
 import useApi from '@ors/hooks/useApi'
 
-function useGetInvoices(year_min: number, year_max: number) {
+function useGetInvoices(year_max: number, year_min: number) {
   const { data, loaded, loading, params, setParams } =
     useApi<ApiReplenishmentInvoices>({
       options: {
         params: {
           hide_no_invoice: true,
           ordering: 'country',
-          year_min: year_min,
           year_max: year_max,
+          year_min: year_min,
         },
         withStoreCache: false,
       },
