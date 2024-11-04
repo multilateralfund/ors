aa1f9aef8f0e2de0d91c864d462b4d50eff9131e
David Batranu <david.batranu@eaudeweb.ro>
2024-11-04 13:53:11 +0200

    Fix edit for BP with no activities.

--- frontend/src/@types/api_bp_get_years.ts
+++ frontend/src/@types/api_bp_get_years.ts
@@ -1,8 +1,10 @@
 // Response from /api/business-plan/get-years
 
 export interface ApiBPYearRange {
-  max_year: number
-  min_year: number
+  /** Activities max year. Null when BP has no activities. */
+  max_year: null | number
+  /** Activities min year. Null when BP has no activities. */
+  min_year: null | number
   year_end: number
   year_start: number
 }
