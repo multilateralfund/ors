46b9b3f4ee637e339c1d4a43aaf45b6da00c7bd0
David Batranu <david.batranu@eaudeweb.ro>
2024-11-28 16:02:36 +0200

    Fix styling.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('next-app')!).render(
+createRoot(document.getElementById('main')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
