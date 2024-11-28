be6b3af5bae6822ed77b6101997809974e27c7da
David Batranu <david.batranu@eaudeweb.ro>
2024-11-28 16:46:03 +0200

    Add Sentry.

--- frontend/src/instrumentation.ts
+++ frontend/src/instrumentation.ts
@@ -1,9 +1,28 @@
-// export async function register() {
-//   if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV === 'production') {
-//     await import('../sentry.server.config');
-//   }
-//
-//   if (process.env.NEXT_RUNTIME === 'edge' && process.env.NODE_ENV === 'production') {
-//     await import('../sentry.edge.config');
-//   }
-// }
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://f69d136467ee88ed69456638e731eb32@sentry.edw.ro/79",
+  integrations: [
+    // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+    integrations: [
+      Sentry.replayIntegration({
+        blockAllMedia: true,
+        // Additional Replay configuration goes in here, for example:
+        maskAllText: true,
+      }),
+    ],
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  // Learn more at
+  // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  // Learn more at
+  // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import './instrumentation.ts'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
