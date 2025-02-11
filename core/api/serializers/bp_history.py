109627b926b879d9f030d41f8f61a972c8c6d4fc
Rares Siteavu <rares.siteavu@yahoo.ro>
2025-02-11 15:07:05 +0200

    More bp history data

--- core/api/serializers/bp_history.py
+++ core/api/serializers/bp_history.py
@@ -21,6 +21,18 @@ class BPHistorySerializer(serializers.ModelSerializer):
         read_only=True,
         source="updated_by.username",
     )
+    updated_by_email = serializers.StringRelatedField(
+        read_only=True,
+        source="updated_by.email",
+    )
+    updated_by_first_name = serializers.StringRelatedField(
+        read_only=True,
+        source="updated_by.first_name",
+    )
+    updated_by_last_name = serializers.StringRelatedField(
+        read_only=True,
+        source="updated_by.last_name",
+    )
 
     class Meta:
         model = BPHistory
@@ -30,5 +42,8 @@ class BPHistorySerializer(serializers.ModelSerializer):
             "created_at",
             "updated_by_id",
             "updated_by_username",
+            "updated_by_email",
+            "updated_by_first_name",
+            "updated_by_last_name",
             "event_description",
         ]
