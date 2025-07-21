644027b967aa4b5036d55569e448f8f7eaea6770
Diana Boiangiu <boiangiudiana58@gmail.com>
2025-07-21 11:40:35 +0300

    Add filter for 'All agencies'

--- core/api/views/agency.py
+++ core/api/views/agency.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics
 
 from core.api.serializers.agency import AgencySerializer
@@ -11,3 +13,28 @@ class AgencyListView(generics.ListAPIView):
 
     queryset = Agency.objects.all()
     serializer_class = AgencySerializer
+
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        include_all_agencies_option = self.request.query_params.get(
+            "include_all_agencies_option", "false"
+        ).lower() == "true"
+
+        if not include_all_agencies_option:
+            queryset = queryset.exclude(name="All agencies")
+        return queryset
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "include_all_agencies_option",
+                openapi.IN_QUERY,
+                description="Includes 'All Agencies' option in the response",
+                type=openapi.TYPE_BOOLEAN,
+                default=False,
+            ),
+        ],
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
