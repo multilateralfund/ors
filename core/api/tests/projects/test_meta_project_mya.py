e83ff393a42d472a060d848334ac6482934b24b0
andreighh <61791875+andreighh@users.noreply.github.com>
2025-09-29 09:43:36 +0300

    Move pyliny comment

--- core/api/tests/projects/test_meta_project_mya.py
+++ core/api/tests/projects/test_meta_project_mya.py
@@ -3,9 +3,11 @@ from django.urls import reverse
 
 from core.api.tests.base import BaseTest
 from core.api.tests.factories import MetaProjectFactory
+
+# pylint: disable=unused-import
 from core.api.tests.projects.test_projects_v2 import (
     setup_project_list,
-)  # pylint: disable=unused-import
+)
 from core.models.project import MetaProject
 from core.models.project import Project
 from core.utils import get_meta_project_code
