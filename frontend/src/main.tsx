33da68d8bfc851b6d489ec5c946ba771c7fe2a7d
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2025-11-05 19:13:11 +0200

    Properly adding frontend sentry settings

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,17 @@ import './instrumentation.ts'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { loadRuntimeConfig } from './config/base'
+import { initializeSentry } from './instrumentation'
 
-createRoot(document.getElementById('main')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+loadRuntimeConfig()
+  .then(() => {
+    initializeSentry()
+  })
+  .finally(() => {
+    createRoot(document.getElementById('main')!).render(
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    )
+  })
