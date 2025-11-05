8555987341fbfcc04b21ecd487746caa73ad253f
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2025-11-05 15:48:04 +0200

    Using env vars for Sentry configuration

--- frontend/src/instrumentation.ts
+++ frontend/src/instrumentation.ts
@@ -1,28 +1,32 @@
 import * as Sentry from '@sentry/react'
 
 if (import.meta.env.PROD) {
-  Sentry.init({
-    dsn: 'https://f69d136467ee88ed69456638e731eb32@sentry.edw.ro/79',
-    integrations: [
-      // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-      Sentry.replayIntegration({
-        blockAllMedia: true,
-        // Additional Replay configuration goes in here, for example:
-        maskAllText: true,
-      }),
-    ],
+  const sentryDsn = import.meta.env.VITE_PUBLIC_SENTRY_DSN
 
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for tracing.
-    // Learn more at
-    // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
-    tracesSampleRate: 1.0,
+  if (sentryDsn) {
+    Sentry.init({
+      dsn: sentryDsn,
+      integrations: [
+        // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+        Sentry.replayIntegration({
+          blockAllMedia: true,
+          // Additional Replay configuration goes in here, for example:
+          maskAllText: true,
+        }),
+      ],
 
-    // Capture Replay for 10% of all sessions,
-    // plus for 100% of sessions with an error
-    // Learn more at
-    // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-  })
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for tracing.
+      // Learn more at
+      // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
+      tracesSampleRate: 1.0,
+
+      // Capture Replay for 10% of all sessions,
+      // plus for 100% of sessions with an error
+      // Learn more at
+      // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
+      replaysSessionSampleRate: 0.1,
+      replaysOnErrorSampleRate: 1.0,
+    })
+  }
 }
