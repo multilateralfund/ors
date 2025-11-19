e884e66fee0e2669e853ba1d9b77d4127e448327
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2025-11-19 15:40:08 +0200

    Fixing the filtersets

--- core/api/filters/annual_project_reports.py
+++ core/api/filters/annual_project_reports.py
@@ -1,4 +1,5 @@
 from django_filters import rest_framework as filters
+from django_filters.fields import CSVWidget
 
 from core.models import Project, Agency, Country, AnnualAgencyProjectReport
 
@@ -38,26 +39,37 @@ class APRGlobalFilter(filters.FilterSet):
 
     agency = filters.ModelMultipleChoiceFilter(
         queryset=Agency.objects.all(),
-        field_name="agency",
+        widget=CSVWidget,
+        distinct=True,
     )
 
     region = filters.ModelMultipleChoiceFilter(
-        queryset=Country.objects.filter(location_type=Country.LocationType.REGION),
-        field_name="project_reports__project__country__parent",
-        distinct=True,
+        queryset=Country.objects.filter(
+            location_type__in=[
+                Country.LocationType.REGION,
+                Country.LocationType.SUBREGION,
+            ]
+        ),
+        to_field_name="name",
+        widget=CSVWidget,
+        method="filter_by_region",
     )
 
     country = filters.ModelMultipleChoiceFilter(
         queryset=Country.objects.filter(location_type=Country.LocationType.COUNTRY),
-        field_name="project_reports__project__country",
+        to_field_name="name",
+        widget=CSVWidget,
         distinct=True,
+        method="filter_by_country",
     )
+
+    cluster = filters.CharFilter(method="filter_by_cluster")
 
     status = filters.CharFilter(method="filter_by_status")
 
     class Meta:
         model = AnnualAgencyProjectReport
-        fields = ["agency", "region", "country", "status"]
+        fields = ["agency", "region", "country", "status", "cluster"]
 
     def filter_by_status(self, queryset, _name, value):
         """
@@ -77,3 +89,32 @@ class APRGlobalFilter(filters.FilterSet):
             return queryset
 
         return queryset.filter(status__in=status_codes)
+
+    def filter_by_region(self, queryset, _name, value):
+        if not value:
+            return queryset
+
+        return queryset.filter(
+            project_reports__project__country__parent__in=value
+        ).distinct()
+
+    def filter_by_country(self, queryset, _name, value):
+        if not value:
+            return queryset
+
+        return queryset.filter(project_reports__project__country__in=value).distinct()
+
+    def filter_by_cluster(self, queryset, _name, value):
+        """
+        Accepts a comma-separated list of cluster *names*.
+        """
+        if not value:
+            return queryset
+
+        cluster_names = [c.strip() for c in value.split(",") if c.strip()]
+        if not cluster_names:
+            return queryset
+
+        return queryset.filter(
+            project_reports__project__cluster__name__in=cluster_names
+        ).distinct()
