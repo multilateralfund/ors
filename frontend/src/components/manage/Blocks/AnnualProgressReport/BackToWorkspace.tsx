c8e3796edfe5f58d36060edfd5e63853f5be0877
andreighh <61791875+andreighh@users.noreply.github.com>
2025-12-10 15:02:09 +0200

    Prevent back to workspace link from taking all width

--- frontend/src/components/manage/Blocks/AnnualProgressReport/BackToWorkspace.tsx
+++ frontend/src/components/manage/Blocks/AnnualProgressReport/BackToWorkspace.tsx
@@ -6,7 +6,7 @@ export default function BackToWorkspace({ year }: { year?: string }) {
   const url = year ? `/${year}/workspace` : '/apr'
 
   return (
-    <Link className="text-black no-underline" href={url}>
+    <Link className="text-black no-underline max-w-fit inline-block" href={url}>
       <div className="mb-2 flex items-center gap-2 text-lg uppercase">
         <IoReturnUpBack size={18} />
         Annual Progress Report Workspace
