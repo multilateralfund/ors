711121a36e2db65e699ae749ac797c305a04e3cf
David Bătrânu <david.batranu@eaudeweb.ro>
2025-12-16 19:55:33 +0200

    [refs #33950] Re-structure output

--- core/api/export/single_project_v2/compare_versions_as_xlsx.py
+++ core/api/export/single_project_v2/compare_versions_as_xlsx.py
@@ -1,3 +1,5 @@
+# pylint: disable=too-many-locals
+from collections import defaultdict
 from itertools import chain
 
 import openpyxl
@@ -18,18 +20,27 @@ from core.api.serializers.project_v2 import ProjectDetailsV2Serializer
 
 from core.api.utils import workbook_response
 from core.api.export.base import configure_sheet_print
+from core.api.export.single_project_v2.helpers import format_iso_date
 
 
-HEADER = [
-    {"id": "metacode", "headerName": "MYA Metacode"},
-    {"id": "code", "headerName": "Project Code"},
-    {"id": "country.name", "headerName": "Country"},
-    {"id": "agency.name", "headerName": "Agency"},
-    {"id": "cluster.name", "headerName": "Cluster"},
-    {"id": "sector.name", "headerName": "Sector"},
-    {"id": "subsectors.name", "headerName": "Subsector"},
-    {"id": "title", "headerName": "Title"},
-]
+SECTIONS = (
+    "Cross-Cutting",
+    "Identifiers",
+    "Header",
+    "Impact",
+    "Substance Details",
+    "Approval",
+    "MYA",
+)
+
+VARIANCE_FIELDS = (
+    "total_fund",
+    "support_cost_psc",
+    "ods_odp.odp",
+    "ods_odp.odp",
+    "ods_odp.phase_out_mt",
+    "ods_odp.co2_mt",
+)
 
 
 def serialize_project(p):
@@ -42,8 +53,8 @@ def version_label(p):
     post_excom_meeting = p.post_excom_meeting.number if p.post_excom_meeting else ""
     meeting = p.meeting.number if p.meeting else ""
     if p.version > 3:
-        return f"{status} (ExCom {post_excom_meeting})"
-    return f"{status} (Meeting {meeting})"
+        return f"Version: {p.version}: {status} (ExCom {post_excom_meeting})"
+    return f"Version: {p.version}: {status} (Meeting {meeting})"
 
 
 class CompareVersionsWriter:
@@ -55,82 +66,110 @@ class CompareVersionsWriter:
         p1, p2 = list(projects)
         d1, d2 = serialize_project(p1), serialize_project(p2)
         l1, l2 = version_label(p1), version_label(p2)
-        headers = [h["headerName"] for h in HEADER]
 
         value_headers = self.get_other_headers(
             self.get_fields(user),
             self.get_specific_information_fields(user),
-            exclude=[h["id"].split(".", maxsplit=1)[0] for h in HEADER],
         )
-        for f in value_headers:
-            headers.append(f["headerName"])
-            headers.extend([None, None])
 
-        self.sheet.append(headers)
-        for i in range(len(HEADER) + 1, len(HEADER) + (len(value_headers) * 3) + 1, 3):
+        per_section_headers = defaultdict(list)
+        for header in value_headers:
+            per_section_headers[header["section"]].append(header)
+
+        h_sections = [None]
+        h_fields = [None]
+        for section in SECTIONS:
+            members = per_section_headers[section]
+            h_sections.extend([section] * len(members))
+            h_fields.extend([m["headerName"] for m in members])
+
+        self.sheet.append(h_sections)
+        self.sheet.append(h_fields)
+
+        self.mark_header_row(1, len(h_sections))
+        self.mark_header_row(2, len(h_fields))
+
+        merge_start_idx = 2
+        for section in SECTIONS:
+            members = per_section_headers[section]
+            merge_end_idx = merge_start_idx + len(members) - 1
             self.sheet.merge_cells(
-                start_row=1, start_column=i, end_row=1, end_column=i + 2
+                start_row=1,
+                start_column=merge_start_idx,
+                end_row=1,
+                end_column=merge_end_idx,
             )
+            merge_start_idx = merge_end_idx + 1
 
-        self.sheet.append(
-            [h["headerName"] for h in HEADER]
-            + ([l1, l2, "Variance"] * len(value_headers))
-        )
-        for i in range(len(HEADER)):
-            self.sheet.merge_cells(
-                start_row=1, start_column=i + 1, end_row=2, end_column=i + 1
-            )
+        for section in SECTIONS:
+            members = per_section_headers[section]
 
-        self.mark_header_rows(headers)
+        data_p1 = [l1]
+        data_p2 = [l2]
+        data_variance = ["Variance"]
 
-        data = []
+        data_remap = {
+            "lead_agency": "lead_agency.name",
+        }
 
-        for h in HEADER:
-            data.append(self.get_value(h["id"]))
+        for section in SECTIONS:
+            members = per_section_headers[section]
+            for h in members:
+                h_id = h["id"]
+                if h_id_ovr := data_remap.get(h_id):
+                    v1 = self.get_value(h_id_ovr, p1)
+                    v2 = self.get_value(h_id_ovr, p2)
+                else:
+                    v1 = self.get_serialized_value(h_id, d1)
+                    v2 = self.get_serialized_value(h_id, d2)
 
-        for h in value_headers:
-            v1 = (
-                h["method"](d1, h)
-                if h.get("method")
-                else self.get_serialized_value(h["id"], d1)
-            )
-            v2 = (
-                h["method"](d2, h)
-                if h.get("method")
-                else self.get_serialized_value(h["id"], d2)
-            )
-            variance = None
+                variance = None
+                if h_id in VARIANCE_FIELDS and v1 and v2:
+                    try:
+                        variance = abs(v2 - v1)
+                    except TypeError:
+                        pass
 
-            if v1 or v2:
-                print(h["id"], v1, v2)
+                if formatter := h.get("formatter"):
+                    v1 = formatter(v1)
+                    v2 = formatter(v2)
 
-            if v1 and v2:
-                try:
-                    variance = abs(v2 - v1)
-                except TypeError:
-                    pass
+                data_p1.append(v1)
+                data_p2.append(v2)
+                data_variance.append(variance)
 
-            data.extend(
-                [
-                    v1,
-                    v2,
-                    variance,
-                ]
-            )
+        for data in [data_p1, data_p2, data_variance]:
+            self.sheet.append(data)
 
-        self.sheet.append(data)
-
-        for i in range(1, len(data) + 1):
+        for i in range(1, len(h_fields) + 1):
             self.sheet.column_dimensions[get_column_letter(i)].width = 20
 
-    def mark_header_rows(self, headers):
-        for i in range(1, len(headers) + 1):
-            for j in range(1, 3):
-                cell = self.sheet.cell(j, i)
-                cell.font = Font(name=DEFAULT_FONT.name, bold=True, color=None)
-                cell.alignment = Alignment(
-                    horizontal="center", vertical="center", wrap_text=True
-                )
+        self.mark_variance_row(5, len(data_variance))
+
+        self.apply_number_format(per_section_headers)
+
+    def apply_number_format(self, per_section_headers):
+        for section in SECTIONS:
+            members = per_section_headers[section]
+            col = 1
+            for h in members:
+                col += 1
+                if cell_format := h.get("cell_format"):
+                    for row in range(3, 6):
+                        self.sheet.cell(row, col).number_format = cell_format
+
+    def mark_header_row(self, row, count):
+        for i in range(1, count + 1):
+            cell = self.sheet.cell(row, i)
+            cell.font = Font(name=DEFAULT_FONT.name, bold=True, color=None)
+            cell.alignment = Alignment(
+                horizontal="center", vertical="center", wrap_text=True
+            )
+
+    def mark_variance_row(self, row, count):
+        for i in range(1, count + 1):
+            cell = self.sheet.cell(row, i)
+            cell.font = Font(name=DEFAULT_FONT.name, bold=False, color="FF0000")
 
     def get_fields(self, user):
         return ProjectField.objects.get_visible_fields_for_user(user).exclude(
@@ -168,14 +207,17 @@ class CompareVersionsWriter:
                 if f.table != "project"
                 else f.read_field_name
             )
-            result.append(
-                {
-                    "id": path,
-                    "headerName": (
-                        f"{f.section}: {f.label}" if f.section != "Header" else f.label
-                    ),
-                }
-            )
+            h_def = {
+                "id": path,
+                "section": f.section,
+                "headerName": f.label,
+            }
+            if f.data_type == "decimal":
+                h_def["cell_format"] = "$###,###,##0.00#############"
+
+            elif f.data_type == "date":
+                h_def["formatter"] = format_iso_date
+            result.append(h_def)
             known_ids.append(f.read_field_name)
 
         return result
@@ -214,6 +256,9 @@ class CompareVersionsWriter:
         elif isinstance(last, (tuple, list, set)):
             last = ", ".join(map(str, last))
 
+        elif isinstance(last, bool):
+            last = "Yes" if last else "No"
+
         return last
 
 
@@ -239,7 +284,5 @@ class CompareVersionsProjectExport:
 
     def export(self):
         CompareVersionsWriter(self.sheet, self.project).write(self.user, self.queryset)
-        filename = (
-            f"Compare versions = {'_'.join([str(p.id) for p in self.queryset])}.xlsx"
-        )
+        filename = f"Compare versions = {'_'.join([str(p.id) for p in self.queryset])}"
         return workbook_response(filename, self.wb)
