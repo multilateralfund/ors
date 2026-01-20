62d42f0de4f66a50a613c61caeb13fd666bb8150
andreighh <61791875+andreighh@users.noreply.github.com>
2026-01-20 17:16:21 +0200

    Add summary tables links in frontend

--- frontend/src/app/annual-project-report/[year]/mlfs/workspace/page.tsx
+++ frontend/src/app/annual-project-report/[year]/mlfs/workspace/page.tsx
@@ -640,7 +640,13 @@ export default function APRMLFSWorkspace() {
               >
                 Update APR (tabs)
               </MlfsLink>
-              <Button variant="text" startIcon={<FiTable size={18} />} disabled>
+              <Button
+                variant="text"
+                startIcon={<FiTable size={18} />}
+                href={formatApiUrl(
+                  `api/annual-project-report/summary-tables/export/`,
+                )}
+              >
                 Generate summary tables
               </Button>
             </div>
