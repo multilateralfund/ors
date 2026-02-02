eca47dc2d6b2edd1a3e7ecd214510e359f890f3d
andreighh <61791875+andreighh@users.noreply.github.com>
2026-02-02 10:11:42 +0200

    Draft pcr view

--- frontend/src/app/project-completion-report/view/page.tsx
+++ frontend/src/app/project-completion-report/view/page.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import PageWrapper from '@ors/components/theme/PageWrapper/PageWrapper.tsx'
+import BackLink from '@ors/components/manage/Blocks/ProjectReport/BackLink.tsx'
+import Loader from '@ors/components/manage/Blocks/ProjectReport/Loader.tsx'
+import { Box } from '@mui/material'
+import { PageHeading } from '@ors/components/ui/Heading/Heading.tsx'
 
 function PCRView() {
-  return <PageWrapper>
-    <div></div>
-  </PageWrapper>
+  return (
+    <PageWrapper>
+      <BackLink url="~/projects-listing" text="IA/BA Portal" />
+      <PageHeading>
+        Project Completion Reports
+      </PageHeading>
+      <Box className="shadow-none">
+        <Loader active={false} />
+      </Box>
+    </PageWrapper>
+  )
 }
 
 export default PCRView
