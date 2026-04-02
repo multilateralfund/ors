b78029b35b6f467ce62538e3e767aebf49843e53
Diana Boiangiu <boiangiudiana58@gmail.com>
2026-04-02 18:40:14 +0300

    Fix black

--- core/migrations/0287_alter_project_funding_window.py
+++ core/migrations/0287_alter_project_funding_window.py
@@ -7,13 +7,20 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0286_fundingwindow_alter_project_funding_window'),
+        ("core", "0286_fundingwindow_alter_project_funding_window"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='project',
-            name='funding_window',
-            field=models.ForeignKey(blank=True, help_text='Funding window', null=True, on_delete=django.db.models.deletion.PROTECT, related_name='projects', to='core.fundingwindow'),
+            model_name="project",
+            name="funding_window",
+            field=models.ForeignKey(
+                blank=True,
+                help_text="Funding window",
+                null=True,
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name="projects",
+                to="core.fundingwindow",
+            ),
         ),
     ]
