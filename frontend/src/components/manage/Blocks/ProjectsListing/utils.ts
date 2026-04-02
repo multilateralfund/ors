4530efc8594c13732933bf64fbceec5db544f766
dana-cfc4 <dana.comiselu4@gmail.com>
2026-04-02 11:54:44 +0300

    Disable cluster editing after creation, remove tansfer validation

--- frontend/src/components/manage/Blocks/ProjectsListing/utils.ts
+++ frontend/src/components/manage/Blocks/ProjectsListing/utils.ts
@@ -793,11 +793,6 @@ export const getTransferErrors = (
     ...(Number(psc_received) > Number(project.support_cost_psc) && {
       psc_received: ['Value cannot be greater than project support costs.'],
     }),
-    ...(Number(psc_received) > Number(psc_transferred) && {
-      psc_received: [
-        'Value cannot be greater than transferred project support costs.',
-      ],
-    }),
     ...(shouldValidateTotalFund &&
       Number(psc_transferred) > Number(fund_transferred) && {
         psc_transferred: [
