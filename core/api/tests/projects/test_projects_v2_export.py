4ba3072c7f528c787889d0c8372d46e5dcb39b94
David Bătrânu <david.batranu@eaudeweb.ro>
2026-04-07 20:10:07 +0300

    Fix empty f-strings (pylint).

--- core/api/tests/projects/test_projects_v2_export.py
+++ core/api/tests/projects/test_projects_v2_export.py
@@ -315,23 +315,23 @@ class TestProjectV2ExportXLSX(BaseTest):
         assert sheet["A5"].value == "Subtotal"
         assert sheet["A6"].value == "Grand total"
 
-        assert sheet[f"D5"].value is None
-        assert sheet[f"G5"].value == "=SUBTOTAL(9,G2:G3)"
-        assert sheet[f"H5"].value == "=SUBTOTAL(9,H2:H3)"
-        assert sheet[f"P5"].value == "=SUBTOTAL(9,P2:P3)"
-        assert sheet[f"S5"].value == "=SUBTOTAL(9,S2:S3)"
-        assert sheet[f"U5"].value == "=SUBTOTAL(9,U2:U3)"
-        assert sheet[f"Y5"].value == "=SUBTOTAL(9,Y2:Y3)"
-        assert sheet[f"H5"].number_format == "$###,###,##0.00#############"
+        assert sheet["D5"].value is None
+        assert sheet["G5"].value == "=SUBTOTAL(9,G2:G3)"
+        assert sheet["H5"].value == "=SUBTOTAL(9,H2:H3)"
+        assert sheet["P5"].value == "=SUBTOTAL(9,P2:P3)"
+        assert sheet["S5"].value == "=SUBTOTAL(9,S2:S3)"
+        assert sheet["U5"].value == "=SUBTOTAL(9,U2:U3)"
+        assert sheet["Y5"].value == "=SUBTOTAL(9,Y2:Y3)"
+        assert sheet["H5"].number_format == "$###,###,##0.00#############"
 
-        assert sheet[f"D6"].value is None
-        assert sheet[f"G6"].value == "=SUM(G2:G3)"
-        assert sheet[f"H6"].value == "=SUM(H2:H3)"
-        assert sheet[f"P6"].value == "=SUM(P2:P3)"
-        assert sheet[f"S6"].value == "=SUM(S2:S3)"
-        assert sheet[f"U6"].value == "=SUM(U2:U3)"
-        assert sheet[f"Y6"].value == "=SUM(Y2:Y3)"
-        assert sheet[f"H6"].number_format == "$###,###,##0.00#############"
+        assert sheet["D6"].value is None
+        assert sheet["G6"].value == "=SUM(G2:G3)"
+        assert sheet["H6"].value == "=SUM(H2:H3)"
+        assert sheet["P6"].value == "=SUM(P2:P3)"
+        assert sheet["S6"].value == "=SUM(S2:S3)"
+        assert sheet["U6"].value == "=SUM(U2:U3)"
+        assert sheet["Y6"].value == "=SUM(Y2:Y3)"
+        assert sheet["H6"].number_format == "$###,###,##0.00#############"
 
 
 class TestProjectV2ExportDOCX(BaseTest):
