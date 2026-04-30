e347c5c0ff17b2cea1df45998ec14cb0439bd880
dana-cfc4 <56276248+dana-cfc4@users.noreply.github.com>
2026-04-30 14:17:39 +0300

    Projects - List of terms tab (#1106)

--- frontend/src/components/manage/Blocks/ProjectsListing/ProjectsSettings/ProjectsSettings.tsx
+++ frontend/src/components/manage/Blocks/ProjectsListing/ProjectsSettings/ProjectsSettings.tsx
@@ -8,7 +8,6 @@ import React, {
 } from 'react'
 import * as mui from '@mui/material'
 import {
-  Box,
   Button,
   Checkbox,
   FormControl,
@@ -20,6 +19,7 @@ import {
 } from '@mui/material'
 
 import api from '@ors/helpers/Api/_api'
+import { formatApiUrl } from '@ors/helpers'
 
 import { useSnackbar } from 'notistack'
 import { useGetProjectSettings } from '@ors/components/manage/Blocks/ProjectsListing/hooks/useGetProjectSettings.ts'
@@ -29,6 +29,7 @@ import {
 } from '@ors/types/api_project_settings.ts'
 import { FormattedNumberInput } from '@ors/components/manage/Blocks/Replenishment/Inputs'
 import { MdOutlineHistory } from 'react-icons/md'
+
 type EmailSettingsType = {
   withNotifications: boolean
   emailAddresses: string
@@ -284,7 +285,7 @@ const ProjectsSettingsEmails = (props: ProjectSettingsParams) => {
           aprAgencySubmitEmail.emailAddresses !==
             apr_agency_submission_notifications_emails)
       return (
-        <Box key={type} className="mt-5">
+        <mui.Box key={type}>
           <form>
             <FormControl className="w-full" error={!!emailSettings.errors}>
               <FormLabel className="text-[#0095D5]">{label}</FormLabel>
@@ -379,7 +380,7 @@ const ProjectsSettingsEmails = (props: ProjectSettingsParams) => {
               )}
             </FormControl>
           </form>
-        </Box>
+        </mui.Box>
       )
     },
   )
@@ -448,7 +449,7 @@ const ProjectsSettingsGlobalFields = ({
     refetch()
   }
   return (
-    <mui.Box className="mt-5">
+    <mui.Box>
       {fields.map((f) => {
         return (
           <div className="my-2 flex items-center">
@@ -481,9 +482,165 @@ const ProjectsSettingsGlobalFields = ({
   )
 }
 
+const ProjectsSettingsListsOfTerms = () => {
+  const firstColumnClassName = 'py-2 pr-6'
+  const secondColumnClassName = 'px-6 py-2'
+
+  return (
+    <mui.Box>
+      <h2 className="mt-2 text-3xl">
+        Management of values for the pick lists used in the IA/BA portal
+      </h2>
+
+      <h2 className="mt-6">Project specific fields</h2>
+
+      <p>
+        Defines impact fields for a given cluster/type/sector combination. Both
+        the planned and actual indicator need to be added.
+      </p>
+      <p>
+        <strong>Warning:</strong> do not add any other fields than the specific
+        ones, because the cross cutting and identifier ones are defined for
+        consistency and implementation purposes. It is best to import the
+        combinations from the respective file.
+      </p>
+
+      <p>
+        These fields can be added from{' '}
+        <a
+          className="italic text-inherit"
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+          href={formatApiUrl('/admin/core/projectspecificfields/')}
+        >
+          /admin/core/projectspecificfields/
+        </a>
+      </p>
+
+      <h2 className="mt-8">Substances</h2>
+
+      <tr>
+        <th className={firstColumnClassName}>Label in application</th>
+        <th className={secondColumnClassName}>Values</th>
+      </tr>
+
+      <tr>
+        <td className={firstColumnClassName}>Substances</td>
+        <td className={secondColumnClassName}>
+          <a
+            className="italic text-inherit"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            href={formatApiUrl('/admin/core/substance/')}
+          >
+            /admin/core/substance/
+          </a>
+        </td>
+      </tr>
+
+      <tr>
+        <td className={firstColumnClassName}>Substance alternative names</td>
+        <td className={secondColumnClassName}>
+          <a
+            className="italic text-inherit"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            href={formatApiUrl('/admin/core/substancealtname/')}
+          >
+            /admin/core/substancealtname/
+          </a>
+        </td>
+      </tr>
+
+      <h2 className="mt-8">
+        Fields that cannot be changed in the Master Database
+      </h2>
+
+      <p>
+        The following fields are defined in the database, but they can only be
+        changed by programmers, because of their impact in the application's
+        logic:
+      </p>
+      <ul>
+        <li>Project statuses</li>
+        <li>Project submission statuses</li>
+      </ul>
+
+      <p>
+        The following fields do not have the values stored in the Master
+        Database; the values are defined in the source code:
+      </p>
+
+      <tr>
+        <th className={firstColumnClassName}>Label in application</th>
+        <th className={secondColumnClassName}>Values</th>
+      </tr>
+      <tr>
+        <td className={firstColumnClassName}>
+          Blanket approval or individual consideration
+        </td>
+        <td className={secondColumnClassName}>
+          Blanket
+          <br />
+          Individual
+        </td>
+      </tr>
+      <tr>
+        <td className={firstColumnClassName}>Consumption level status</td>
+        <td className={secondColumnClassName}>
+          LVC
+          <br />
+          Non-LVC
+          <br />
+          N/A
+        </td>
+      </tr>
+      <tr>
+        <td className={firstColumnClassName}>Production control type</td>
+        <td className={secondColumnClassName}>
+          Reduction
+          <br />
+          Closure
+          <br />
+          Switch to production for feedstock uses
+          <br />
+          Conversion to non-controlled substance
+          <br />
+          Other
+        </td>
+      </tr>
+      <tr>
+        <td className={firstColumnClassName}>Category</td>
+        <td className={secondColumnClassName}>
+          Multi-year agreement
+          <br />
+          Individual
+        </td>
+      </tr>
+      <tr>
+        <td className={firstColumnClassName}>Attachments → Type</td>
+        <td className={secondColumnClassName}>
+          Project proposal
+          <br />
+          Endorsement/transmittal letter from government
+          <br />
+          Verification report
+          <br />
+          Project review comments
+          <br />
+          Transferred project proposal
+          <br />
+          Other
+        </td>
+      </tr>
+    </mui.Box>
+  )
+}
+
 enum Tab {
   Emails = 0,
   GlobalFields = 1,
+  ListsOfTerms = 2,
 }
 
 const ProjectsSettings = ({ data, refetch }: ProjectSettingsParams) => {
@@ -494,13 +651,28 @@ const ProjectsSettings = ({ data, refetch }: ProjectSettingsParams) => {
         return <ProjectsSettingsEmails data={data} refetch={refetch} />
       case Tab.GlobalFields:
         return <ProjectsSettingsGlobalFields data={data} refetch={refetch} />
+      case Tab.ListsOfTerms:
+        return <ProjectsSettingsListsOfTerms />
     }
   }, [currentTab, data, refetch])
   return (
     <div>
-      <mui.Tabs value={currentTab} onChange={(_, v) => setCurrentTab(v)}>
+      <mui.Tabs
+        aria-label="settings-tabs"
+        value={currentTab}
+        className="sectionsTabs mt-6"
+        variant="scrollable"
+        scrollButtons="auto"
+        allowScrollButtonsMobile
+        TabIndicatorProps={{
+          className: 'h-0',
+          style: { transitionDuration: '150ms' },
+        }}
+        onChange={(_, v) => setCurrentTab(v)}
+      >
         <mui.Tab label="Emails" />
         <mui.Tab label="Global fields" />
+        <mui.Tab label="Lists of terms" />
       </mui.Tabs>
       {tabContent}
     </div>
