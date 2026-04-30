f45ea48f2c2bacf284e9f63dbb80db87c9d44d05
dana-cfc4 <dana.comiselu4@gmail.com>
2026-04-30 14:07:11 +0300

    Better styling

--- frontend/src/components/manage/Blocks/ProjectsListing/ProjectsSettings/ProjectsSettings.tsx
+++ frontend/src/components/manage/Blocks/ProjectsListing/ProjectsSettings/ProjectsSettings.tsx
@@ -19,6 +19,7 @@ import {
 } from '@mui/material'
 
 import api from '@ors/helpers/Api/_api'
+import { formatApiUrl } from '@ors/helpers'
 
 import { useSnackbar } from 'notistack'
 import { useGetProjectSettings } from '@ors/components/manage/Blocks/ProjectsListing/hooks/useGetProjectSettings.ts'
@@ -28,7 +29,7 @@ import {
 } from '@ors/types/api_project_settings.ts'
 import { FormattedNumberInput } from '@ors/components/manage/Blocks/Replenishment/Inputs'
 import { MdOutlineHistory } from 'react-icons/md'
-import { formatApiUrl } from '@ors/helpers'
+
 type EmailSettingsType = {
   withNotifications: boolean
   emailAddresses: string
@@ -481,155 +482,160 @@ const ProjectsSettingsGlobalFields = ({
   )
 }
 
-const ProjectsSettingsListsOfTerms = () => (
-  <mui.Box>
-    <h2 className="mt-2 text-3xl">
-      Management of values for the pick lists used in the IA/BA portal
-    </h2>
+const ProjectsSettingsListsOfTerms = () => {
+  const firstColumnClassName = 'py-2 pr-6'
+  const secondColumnClassName = 'px-6 py-2'
 
-    <h2 className="mt-6">Project specific fields</h2>
+  return (
+    <mui.Box>
+      <h2 className="mt-2 text-3xl">
+        Management of values for the pick lists used in the IA/BA portal
+      </h2>
 
-    <p>
-      Defines impact fields for a given cluster/type/sector combination. Both
-      the planned and actual indicator need to be added.
-    </p>
-    <p>
-      <strong>Warning:</strong> do not add any other fields than the specific
-      ones, because the cross cutting and identifier ones are defined for
-      consistency and implementation purposes. It is best to import the
-      combinations from the respective file.
-    </p>
+      <h2 className="mt-6">Project specific fields</h2>
 
-    <p>
-      These fields can be added from{' '}
-      <a
-        className="italic text-inherit"
-        target="_blank"
-        rel="noopener noreferrer nofollow"
-        href={formatApiUrl('/admin/core/projectspecificfields/')}
-      >
-        /admin/core/projectspecificfields/
-      </a>
-    </p>
+      <p>
+        Defines impact fields for a given cluster/type/sector combination. Both
+        the planned and actual indicator need to be added.
+      </p>
+      <p>
+        <strong>Warning:</strong> do not add any other fields than the specific
+        ones, because the cross cutting and identifier ones are defined for
+        consistency and implementation purposes. It is best to import the
+        combinations from the respective file.
+      </p>
 
-    <h2 className="mt-8">Substances</h2>
-
-    <tr>
-      <th className="py-2 pr-6">Label in application</th>
-      <th className="px-6 py-2">Values</th>
-    </tr>
-
-    <tr>
-      <td className="py-2 pr-6">Substances</td>
-      <td className="px-6 py-2">
+      <p>
+        These fields can be added from{' '}
         <a
           className="italic text-inherit"
           target="_blank"
           rel="noopener noreferrer nofollow"
-          href={formatApiUrl('/admin/core/substance/')}
+          href={formatApiUrl('/admin/core/projectspecificfields/')}
         >
-          /admin/core/substance/
+          /admin/core/projectspecificfields/
         </a>
-      </td>
-    </tr>
+      </p>
 
-    <tr>
-      <td className="py-2 pr-6">Substance alternative names</td>
-      <td className="px-6 py-2">
-        <a
-          className="italic text-inherit"
-          target="_blank"
-          rel="noopener noreferrer nofollow"
-          href={formatApiUrl('/admin/core/substancealtname/')}
-        >
-          /admin/core/substancealtname/
-        </a>
-      </td>
-    </tr>
+      <h2 className="mt-8">Substances</h2>
 
-    <h2 className="mt-8">
-      Fields that cannot be changed in the Master Database
-    </h2>
+      <tr>
+        <th className={firstColumnClassName}>Label in application</th>
+        <th className={secondColumnClassName}>Values</th>
+      </tr>
 
-    <p>
-      The following fields are defined in the database, but they can only be
-      changed by programmers, because of their impact in the application's
-      logic:
-    </p>
-    <ul>
-      <li>Project statuses</li>
-      <li>Project submission statuses</li>
-    </ul>
+      <tr>
+        <td className={firstColumnClassName}>Substances</td>
+        <td className={secondColumnClassName}>
+          <a
+            className="italic text-inherit"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            href={formatApiUrl('/admin/core/substance/')}
+          >
+            /admin/core/substance/
+          </a>
+        </td>
+      </tr>
 
-    <p>
-      The following fields do not have the values stored in the Master Database;
-      the values are defined in the source code:
-    </p>
+      <tr>
+        <td className={firstColumnClassName}>Substance alternative names</td>
+        <td className={secondColumnClassName}>
+          <a
+            className="italic text-inherit"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            href={formatApiUrl('/admin/core/substancealtname/')}
+          >
+            /admin/core/substancealtname/
+          </a>
+        </td>
+      </tr>
 
-    <tr>
-      <th className="py-2 pr-10">Label in application</th>
-      <th className="py-2 pr-10">Values</th>
-    </tr>
-    <tr>
-      <td className="py-2 pr-10">
-        Blanket approval or individual consideration
-      </td>
-      <td className="py-2 pr-10">
-        Blanket
-        <br />
-        Individual
-      </td>
-    </tr>
-    <tr>
-      <td className="py-2 pr-10">Consumption level status</td>
-      <td className="py-2 pr-10">
-        LVC
-        <br />
-        Non-LVC
-        <br />
-        N/A
-      </td>
-    </tr>
-    <tr>
-      <td className="py-2 pr-10">Production control type</td>
-      <td className="py-2 pr-10">
-        Reduction
-        <br />
-        Closure
-        <br />
-        Switch to production for feedstock uses
-        <br />
-        Conversion to non-controlled substance
-        <br />
-        Other
-      </td>
-    </tr>
-    <tr>
-      <td className="py-2 pr-10">Category</td>
-      <td className="py-2 pr-10">
-        Multi-year agreement
-        <br />
-        Individual
-      </td>
-    </tr>
-    <tr>
-      <td className="py-2 pr-10">Attachments → Type</td>
-      <td className="py-2 pr-10">
-        Project proposal
-        <br />
-        Endorsement/transmittal letter from government
-        <br />
-        Verification report
-        <br />
-        Project review comments
-        <br />
-        Transferred project proposal
-        <br />
-        Other
-      </td>
-    </tr>
-  </mui.Box>
-)
+      <h2 className="mt-8">
+        Fields that cannot be changed in the Master Database
+      </h2>
+
+      <p>
+        The following fields are defined in the database, but they can only be
+        changed by programmers, because of their impact in the application's
+        logic:
+      </p>
+      <ul>
+        <li>Project statuses</li>
+        <li>Project submission statuses</li>
+      </ul>
+
+      <p>
+        The following fields do not have the values stored in the Master
+        Database; the values are defined in the source code:
+      </p>
+
+      <tr>
+        <th className={firstColumnClassName}>Label in application</th>
+        <th className={secondColumnClassName}>Values</th>
+      </tr>
+      <tr>
+        <td className={firstColumnClassName}>
+          Blanket approval or individual consideration
+        </td>
+        <td className={secondColumnClassName}>
+          Blanket
+          <br />
+          Individual
+        </td>
+      </tr>
+      <tr>
+        <td className={firstColumnClassName}>Consumption level status</td>
+        <td className={secondColumnClassName}>
+          LVC
+          <br />
+          Non-LVC
+          <br />
+          N/A
+        </td>
+      </tr>
+      <tr>
+        <td className={firstColumnClassName}>Production control type</td>
+        <td className={secondColumnClassName}>
+          Reduction
+          <br />
+          Closure
+          <br />
+          Switch to production for feedstock uses
+          <br />
+          Conversion to non-controlled substance
+          <br />
+          Other
+        </td>
+      </tr>
+      <tr>
+        <td className={firstColumnClassName}>Category</td>
+        <td className={secondColumnClassName}>
+          Multi-year agreement
+          <br />
+          Individual
+        </td>
+      </tr>
+      <tr>
+        <td className={firstColumnClassName}>Attachments → Type</td>
+        <td className={secondColumnClassName}>
+          Project proposal
+          <br />
+          Endorsement/transmittal letter from government
+          <br />
+          Verification report
+          <br />
+          Project review comments
+          <br />
+          Transferred project proposal
+          <br />
+          Other
+        </td>
+      </tr>
+    </mui.Box>
+  )
+}
 
 enum Tab {
   Emails = 0,
