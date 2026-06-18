e14aad568ee4dc18dd32dc197f02fddf64721b32
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2026-06-18 17:21:55 +0300

    Fixing migration

--- core/migrations/0302_alter_annualprojectreport_gender_policy.py
+++ core/migrations/0302_alter_annualprojectreport_gender_policy.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0300_alter_country_abbr_for_apr_and_more'),
+        ('core', '0301_alter_project_substance_type'),
     ]
 
     operations = [
