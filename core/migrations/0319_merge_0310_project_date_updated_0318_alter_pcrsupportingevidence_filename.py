29fd421678eb61845713519ccb4114f41ef58cdd
David Bătrânu <david.batranu@eaudeweb.ro>
2026-07-31 21:01:53 +0300

    Add no-op migration after staging merge.

---
+++ core/migrations/0319_merge_0310_project_date_updated_0318_alter_pcrsupportingevidence_filename.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0310_project_date_updated"),
+        ("core", "0318_alter_pcrsupportingevidence_filename"),
+    ]
+
+    operations = []
