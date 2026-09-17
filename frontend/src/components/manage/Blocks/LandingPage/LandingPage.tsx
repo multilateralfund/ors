545d8ed9db7e81757bb4813095cfd48594091cbc
dana-cfc4 <dana.comiselu4@gmail.com>
2026-09-17 11:40:21 +0300

    Remove apr from landing page

--- frontend/src/components/manage/Blocks/LandingPage/LandingPage.tsx
+++ frontend/src/components/manage/Blocks/LandingPage/LandingPage.tsx
@@ -13,7 +13,6 @@ const LandingPage = () => {
     canEditReplenishment,
     canViewBp,
     canViewProjects,
-    canViewAPR,
   } = useContext(PermissionsContext)
 
   const cards = [
@@ -43,13 +42,6 @@ const LandingPage = () => {
       description:
         'Discover how Multilateral Fund projects are making a measurable impact in 144 countries. Access key data and performance metrics.',
       permissions: canViewProjects,
-    },
-    {
-      title: 'Annual progress reports',
-      href: '/apr',
-      description:
-        'Review how agencies performed against their Business Plan targets, with project implementation status reported annually.',
-      permissions: canViewAPR,
     },
   ]
 
